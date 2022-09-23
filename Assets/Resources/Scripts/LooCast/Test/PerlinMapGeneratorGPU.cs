bd4b5870547b6c27adb73c083e25e2c4e0a80bfb
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2022-09-24 01:32:42 +0200

    WIP Voronoi Generator for GPU & Repo Fix

--- Assets/Resources/Scripts/LooCast/Test/PerlinMapGeneratorGPU.cs
+++ Assets/Resources/Scripts/LooCast/Test/PerlinMapGeneratorGPU.cs
@@ -1,30 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace LooCast.Test
 {
-    using LooCast.Util;
-    using LooCast.Noise;
+    using Noise;
+    using Util;
 
-    public class VoronoiMapGeneratorGPU : MonoBehaviour
+    public class PerlinMapGenerator : MonoBehaviour
     {
+        public enum DrawMode
+        {
+            NoiseMap,
+            ColorMap
+        }
+        public DrawMode CurrentDrawMode;
+
         public int MapWidth;
         public int MapHeight;
-        public Vector2Int SampleCellAmount;
-        [Range(0.0f, 2.0f)]
-        public float CellSpread;
-        public float Power;
-        public float Amplitude;
+        public float NoiseScale;
+        public float NoiseAmplitude;
+        public int Octaves;
+        [Range(0.0f, 1.0f)]
+        public float Persistence;
+        public float Lacunarity;
+
         [Range(0, 2109876543)]
         public int Seed;
-
+        public Vector2 Offset;
         public bool AutoUpdate;
+        public TerrainType[] regions;
 
         public ComputeShader ComputeShader;
 
         public void GenerateMap()
         {
+            #region OLD DUSTY ASS CPU ALGORITHM
+            float[,] noiseMap = PerlinNoise.GenerateNoiseMap(MapWidth, MapHeight, Seed, NoiseScale, Octaves, Persistence, Lacunarity, NoiseAmplitude, Offset).Array2D;
+
+            Color[] colorMap = new Color[MapWidth * MapHeight];
+            for (int y = 0; y < MapHeight; y++)
+            {
+                for (int x = 0; x < MapWidth; x++)
+                {
+                    float currentHeight = noiseMap[x, y];
+                    for (int i = 0; i < regions.Length; i++)
+                    {
+                        if (currentHeight <= regions[i].height)
+                        {
+                            colorMap[y * MapWidth + x] = regions[i].color;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            MapDisplay display = GetComponent<MapDisplay>();
+            if (CurrentDrawMode == DrawMode.NoiseMap)
+            {
+                display.DrawTexture(TextureUtil.TextureFromHeightMap(noiseMap));
+            }
+
+            else if (CurrentDrawMode == DrawMode.ColorMap)
+            {
+                display.DrawTexture(TextureUtil.TextureFromColorMap(colorMap, MapWidth, MapHeight));
+            }
+            #endregion
+
             #region Voronoi Cell Compute Buffer Creation
             Vector2Int[] voronoiCells = VoronoiNoise.GetCentroids(Seed, MapWidth, MapHeight, SampleCellAmount, CellSpread);
             VoronoiCell[] voronoiCellsData = new VoronoiCell[voronoiCells.Length];
@@ -86,113 +126,79 @@ namespace LooCast.Test
             #endregion
         }
 
-        private float GetMaxDistance(float[] distances)
-        {
-            float maxDistance = float.MinValue;
-            for (int i = 0; i < distances.Length; i++)
-            {
-                if (distances[i] > maxDistance)
-                {
-                    maxDistance = distances[i];
-                }
-            }
-            return maxDistance;
-        }
-
-        private struct VoronoiCell
-        {
-            int index;
-            int positionX;
-            int positionY;
-
-            public VoronoiCell(int index, int positionX, int positionY)
-            {
-                this.index = index;
-                this.positionX = positionX;
-                this.positionY = positionY;
-            }
-
-            public static int ByteSize
-            {
-                get
-                {
-                    return sizeof(int) * 3;
-                }
-            }
-        }
-
-        private struct VoronoiPixel
-        {
-            public float ClosestVoronoiCellIndex
-            {
-                get
-                {
-                    return closestVoronoiCellIndex;
-                }
-            }
-            public float DistanceToVoronoiCell
-            {
-                get
-                {
-                    return distanceToVoronoiCell;
-                }
-            }
-
-            private int positionX;
-            private int positionY;
-            private int closestVoronoiCellIndex;
-            private float distanceToVoronoiCell;
-
-            public VoronoiPixel(int positionX, int positionY)
-            {
-                this.positionX = positionX;
-                this.positionY = positionY;
-                closestVoronoiCellIndex = 0;
-                distanceToVoronoiCell = 0.0f;
-            }
-
-            public static int ByteSize
-            {
-                get
-                {
-                    return (sizeof(int) * 3) + (sizeof(float));
-                }
-            }
-        }
-
         private void OnValidate()
         {
-            if ((MapWidth % 64) != 0)
+            if ((MapWidth % 32) != 0)
             {
-                MapWidth -= (MapWidth % 64);
+                MapWidth -= (MapWidth % 32);
             }
-            if (MapWidth < 64)
+            if (MapWidth < 32)
             {
-                MapWidth = 64;
+                MapWidth = 32;
             }
 
-            if ((MapHeight % 64) != 0)
+            if ((MapHeight % 32) != 0)
             {
-                MapHeight -= (MapHeight % 64);
+                MapHeight -= (MapHeight % 32);
             }
-            if (MapHeight < 64)
+            if (MapHeight < 32)
             {
-                MapHeight = 64;
+                MapHeight = 32;
             }
 
             if (Seed < 0)
             {
                 Seed = Mathf.Abs(Seed);
             }
-            
-            if (SampleCellAmount.x < 1)
+
+            if (MapWidth < 1)
             {
-                SampleCellAmount.x = 1;
+                MapWidth = 1;
             }
-            if (SampleCellAmount.y < 1)
+            if (MapHeight < 1)
             {
-                SampleCellAmount.y = 1;
+                MapHeight = 1;
+            }
+            if (Lacunarity < 1)
+            {
+                Lacunarity = 1;
+            }
+            if (Octaves < 0)
+            {
+                Octaves = 0;
+            }
+        }
+    } 
+
+    #region Structs
+    [System.Serializable]
+    public struct TerrainType
+    {
+        public string name;
+        public float height;
+        public Color color;
+    }
+
+    private struct PerlinPixel
+    {
+        private int positionX;
+        private int positionY;
+        private float perlinValue;
+
+        public VoronoiPixel(int positionX, int positionY)
+        {
+            this.positionX = positionX;
+            this.positionY = positionY;
+            perlinValue = 0.0f;
+        }
+
+        public static int ByteSize
+        {
+            get
+            {
+                return (sizeof(int) * 2) + (sizeof(float));
             }
         }
     }
+    #endregion
 }
