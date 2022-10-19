967c1c6c25a1ba02ea13c6961055dd33a68c35f4
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2022-10-20 01:14:46 +0200

    Fixed Energy Bar

--- Assets/Resources/Scripts/LooCast/UI/Bar/EnergyBar.cs
+++ Assets/Resources/Scripts/LooCast/UI/Bar/EnergyBar.cs
@@ -5,20 +5,13 @@ using UnityEngine.UI;
 
 namespace LooCast.UI.Bar
 {
-    using LooCast.Movement.Data;
     using LooCast.Movement.Data.Runtime;
 
     public class EnergyBar : Bar
     {
-        public Image SliderImage { get; protected set; }
-        public Image BorderImage { get; protected set; }
         public PlayerMovementRuntimeData PlayerMovementRuntimeData;
 
-        private void Start()
-        {
-            SliderImage = transform.Find("SliderImage").GetComponent<Image>();
-            BorderImage = transform.Find("BorderImage").GetComponent<Image>();
-        }
+        [SerializeField] private Image sliderImage;
 
         public override void Refresh()
         {
@@ -47,11 +40,11 @@ namespace LooCast.UI.Bar
                 isDepleted = value;
                 if (isDepleted)
                 {
-                    SliderImage.color = Color.yellow;
+                    sliderImage.color = Color.yellow;
                 }
                 else
                 {
-                    SliderImage.color = Color.green;
+                    sliderImage.color = Color.green;
                 }
             }
         }
