2fdfaabb20b0e97984f562096701d4a45f7f3165
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2022-10-20 00:53:07 +0200

    Added Ally Spawner to Ally Stations; Tweaked Ally & Enemy colors; Tweaked Min and Max Station count in Station Generators

--- Assets/Resources/Scripts/LooCast/Spawner/EnemySpawner.cs
+++ Assets/Resources/Scripts/LooCast/Spawner/EnemySpawner.cs
@@ -6,6 +6,7 @@ namespace LooCast.Spawner
 {
     using Data;
     using Enemy;
+    using Random;
 
     public class EnemySpawner : Spawner
     {
@@ -14,7 +15,7 @@ namespace LooCast.Spawner
         public float SpawnDelay { get; protected set; }
         public float SpawnTimer { get; protected set; }
         public int MaxEnemies { get; protected set; }
-        public GameObject Prefab { get; protected set; }
+        public GameObject EnemyPrefab { get; protected set; }
         public List<Enemy> SpawnedEnemies { get; protected set; }
 
         private void Start()
@@ -24,7 +25,7 @@ namespace LooCast.Spawner
             SpawnDelay = Data.BaseSpawnDelay.Value;
             SpawnTimer = 0.0f;
             MaxEnemies = Data.BaseMaxEnemies.Value;
-            Prefab = Data.EnemyPrefab;
+            EnemyPrefab = Data.EnemyPrefab;
             SpawnedEnemies = new List<Enemy>();
         }
 
@@ -35,7 +36,7 @@ namespace LooCast.Spawner
             if (SpawnTimer >= SpawnDelay && SpawnedEnemies.Count < MaxEnemies)
             {
                 SpawnTimer = 0.0f;
-                GameObject spawnedObject = Instantiate(Prefab, (Vector3)(UnityEngine.Random.insideUnitCircle * 50.0f) + transform.position, Quaternion.identity, null);
+                GameObject spawnedObject = Instantiate(EnemyPrefab, (Vector3)(Random.InsideUnitCircle() * 50.0f) + transform.position, Quaternion.identity, null);
                 Enemy spawnedEnemy = spawnedObject.GetComponent<Enemy>();
                 SpawnedEnemies.Add(spawnedEnemy);
                 spawnedEnemy.OnKilled.AddListener( () => { SpawnedEnemies.Remove(spawnedEnemy); } );
