52e0fa58fd11a38eb83647119f0e1dbcf0bb2b22
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2022-10-19 23:51:08 +0200

    Fixed/Improved Background and some other visual bugs

--- Assets/Resources/Scripts/LooCast/Background/Background.cs
+++ Assets/Resources/Scripts/LooCast/Background/Background.cs
@@ -9,22 +9,13 @@ namespace LooCast.Background
     public class Background : ExtendedMonoBehaviour
     {
         [SerializeField] private Sprite backgroundSprite;
-        private SpriteRenderer[,] backgroundSprites = new SpriteRenderer[3, 3];
+        [SerializeField] private SpriteRenderer[] backgroundRenderers;
 
         private void Start()
         {
-            for (int x = -1; x < 2; x++)
+            for (int i = 0; i < backgroundRenderers.Length; i++)
             {
-                for (int y = -1; y < 2; y++)
-                {
-                    var obj = new GameObject();
-                    obj.transform.parent = transform;
-                    obj.transform.position = new Vector3(x * 128, y * 72, 10);
-                    obj.name = $"Background@y:{x},x:{y}";
-                    var renderer = obj.AddComponent<SpriteRenderer>();
-                    renderer.sprite = backgroundSprite;
-                    backgroundSprites[x + 1, y + 1] = renderer;
-                }
+                backgroundRenderers[i].sprite = backgroundSprite;
             }
         }
 
@@ -32,19 +23,19 @@ namespace LooCast.Background
         {
             Vector3 cameraPos = Camera.main.transform.position;
             Vector2 shift = Vector2.zero;
-            if (cameraPos.x > backgroundSprites[1, 1].transform.position.x + 64)
+            if (cameraPos.x > transform.position.x + 64)
             {
                 shift.x = 1;
             }
-            else if (cameraPos.x < backgroundSprites[1, 1].transform.position.x - 64)
+            else if (cameraPos.x < transform.position.x - 64)
             {
                 shift.x = -1;
             }
-            if (cameraPos.y > backgroundSprites[1, 1].transform.position.y + 36)
+            if (cameraPos.y > transform.position.y + 36)
             {
                 shift.y = 1;
             }
-            else if (cameraPos.y < backgroundSprites[1, 1].transform.position.y - 36)
+            else if (cameraPos.y < transform.position.y - 36)
             {
                 shift.y = -1;
             }
@@ -54,7 +45,7 @@ namespace LooCast.Background
 
             if (shift.x != 0 || shift.y != 0)
             {
-                transform.position = new Vector3(transform.position.x + shift.x, transform.position.y + shift.y, 10);
+                transform.position = new Vector3(transform.position.x + shift.x, transform.position.y + shift.y, transform.position.z);
             }
         }
     } 
