55eca9d248e05b420d3358161f51084a71421eab
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2022-11-11 15:35:08 +0100

    Revert "WIP"
    This reverts commit 1334337eb69d5c72c321ac500fc9bd4f76b0c198.

--- Assets/Resources/Scripts/LooCast/Item/ItemContainer.cs
+++ Assets/Resources/Scripts/LooCast/Item/ItemContainer.cs
@@ -229,14 +229,6 @@ namespace LooCast.Item
             {
                 throw new ArgumentOutOfRangeException($"Invalid slot! Slot must be between 0 {itemSlots.Count - 1}!");
             }
-            if (item.ItemContainmentState == Item.ContainmentState.Contained)
-            {
-                item.UncontainItem();
-            }
-            else if (item.ItemContainmentState == Item.ContainmentState.Dropped)
-            {
-                item.UndropItem();
-            }
             item.ContainItem(this);
             itemSlots[slotID].ItemContent = item;
         }
