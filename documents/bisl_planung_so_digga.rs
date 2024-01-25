15de43f4098a5d8f4275ea685e4bd6044eaf3c50
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2024-01-25 14:01:18 +0100

    Bisl Planung so digga

---
+++ documents/bisl_planung_so_digga.rs
@@ -0,0 +1,151 @@
+The problem with the current Game/Universe/Player modules is that they strongly depend on each other, discouraging extensibility and felxibility.
+The goal of this "bisl_planung_so_digga.rs" is to provide detailed comments and rust pseudocode for a completely re-imagined set of Game/Universe/Player modules.
+These re-imagined modules will focus on their respective responsibilities and nothing more.
+State (like the universe being Loaded, NotLoaded, etc.) should ideally be represented by bevy states; this way we can define startup, update and shutdown systems 
+for whatever event we like, without strongly coupling these 3 modules to themselves and other modules.
+
+
+Basic concepts (in order of startup):
+
+Game(GameInfo, GameConfig, GameState), 
+Universe(GlobalUniverse, LocalUniverses), 
+Player
+
+
+Basic lifecycle example
+
+(CreateGame)
+SelectGame
+	SetCurrentGameInfo
+LoadSelectedGame
+	LoadCurrentGameConfig
+	LoadCurrentGameState
+LoadSelectedUniverse
+
+
+Bit o' rust code, innit mate
+
+pub struct GameInfo {
+	game_id: GameID,
+	name: String,
+	game_config_info: GameConfigInfo,
+	game_state_info: GameStateInfo,
+	global_universe_info: GlobalUniverseInfo,
+	local_universe_infos: Vec<LocalUniverseInfo>,
+}
+
+pub enum GameConfig {}
+
+pub struct GameConfigHandle {
+	game_config: GameConfig
+}
+
+pub struct GameState {}
+
+pub struct GameStateHandle {
+	game_state: GameState
+}
+
+enum Game {
+	GameInfoLoaded {
+		game_info: GameInfo,
+	},
+	GameConfigLoaded {
+		game_info: GameInfo,
+		game_config: GameConfig,
+	},
+	GameStateLoaded {
+		game_info: GameInfo,
+		game_config: GameConfig,
+		game_state: GameState,
+	}
+}
+
+pub struct GameHandle {
+	game: Game
+}
+
+impl struct GameHandle {
+	pub fn get_game_info() -> Result<GameHandleGetInfoError, &GameInfo> {}
+	pub fn get_config_handle() -> Result<GameHandleGetConfigHandleError, GameConfigHandle> {}
+	pub fn get_state_handle() -> Result<GameHandleGetStateHandleError, GameStateHandle>
+}
+
+pub struct GlobalUniverseInfo {
+	id: u64
+}
+
+pub struct LocalUniverseInfo {
+	id: u64,
+	player_info: PlayerInfo,
+}
+
+pub struct ChunkInfo {
+	id: ChunkID,
+}
+
+pub struct ChunkMetadata {
+	parent_chunk_info: ChunkInfo
+	child_chunk_infos: Vec<ChunkInfo>
+	child_entity_infos: Vec<EntityInfo>
+}
+
+pub struct ChunkData {
+	parent_chunk_metadata: ChunkMetadata,
+	child_chunk_metadatas: Vec<Chunk>
+}
+
+pub enum Chunk {
+	ChunkInfoLoaded {
+		chunk_info: ChunkInfo
+	},
+	ChunkMetadataLoaded {
+		
+	},
+	ChunkDataLoaded {
+		
+	},
+}
+
+pub struct EntityInfo {
+	id: EntityID
+}
+
+pub struct EntityMetadata {
+	
+}
+
+pub struct EntityData {
+	
+}
+
+pub enum Entity {
+	EntityInfoLoaded {
+		
+	},
+	EntityMetadataLoaded {
+		
+	},
+	EntityDataLoaded {
+		
+	},
+}
+
+pub struct GameManager {
+	
+}
+
+pub struct UniverseManager {
+	
+}
+
+pub struct PlayerManager {
+	
+}
+
+// Implementations
+impl GameInfo {
+	pub(in crate::spacetime_engine::system) fn new() {
+		
+	}
+}
