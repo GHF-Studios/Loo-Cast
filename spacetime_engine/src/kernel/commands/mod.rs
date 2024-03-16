ca111ce9c1f7e047e02c5b1cbb5f0a2d31855b6e
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2024-03-16 01:31:34 +0100

    WIP Commands Framework

--- spacetime_engine/src/kernel/commands/mod.rs
+++ spacetime_engine/src/kernel/commands/mod.rs
@@ -1,31 +1,219 @@
-// Type: Struct
-// Naming Convention: "[MODULE_NAME]Commands"
-pub trait CommandModule {
+use std::fmt::{Error, Display};
+
+#[macro_export]
+macro_rules! define_commands_module {
+    ($(&tokens:tt)*) => {
+    };
+}
+
+define_commands_module! {
+    Test: { 
+        commands: [
+            HelloWorld: {
+                Input: {
+                    value: i32,
+                },
+                Output: {
+                    value: i32,
+                },
+                Error: {
+                    InvalidInput,
+                },
+                Code: {
+                    closure: (input) => {
+                        match input.value {
+                            0 => {
+                                Ok(HelloWorldCommandOutput {
+                                    value: input.value,
+                                })
+                            },
+                            _ => {
+                                Err(HelloWorldCommandError::InvalidInput)
+                            },
+                        }
+                    },
+                }
+            },
+            DrawGizmoLine: {
+                Input: {
+                    some_value: i32,
+                    some_value: i8,
+                },
+                Output: {
+                    this_value: i32,
+                    that_value: i16,
+                },
+                Error: {
+                    NameIsEmpty,
+                },
+                Code: {
+                    closure: (input) => {
+                        match input.value {
+                            0 => {
+                                Ok(DrawGizmoLineCommandOutput {
+                                    value: input.value,
+                                })
+                            },
+                            _ => {
+                                Err(DrawGizmoLineCommandError::NameIsEmpty)
+                            },
+                        }
+                    },
+                }
+            }
+        ],
+    }
+}
+
+
+
+// This does not belong to the Test Command Module, but is a general Commands-related concept. The TestCommand Module's presence is to demonstrate the concept of a Command Module.
+pub trait CommandsModule {
     fn module_name() -> &'static str;
 }
 
-// Type: Struct
-// Naming Convention: "[COMMAND_NAME]Command"
-pub trait Command {
-    type Module: CommandModule;
+pub(in crate::kernel::commands) trait TestCommand {
+    type Module: CommandsModule;
+    type Input: TestCommandInput<Command = Self>;
+    type Output: TestCommandOutput<Command = Self>;
+    type Error: TestCommandError<Command = Self>;
 
-    fn execute(&self);
+    fn initialize(input: Self::Input) -> Self;
+    fn execute(&mut self);
+    fn finalize(self) -> Option<Result<Self::Output, Self::Error>>;
 }
 
-// Type: Enum
-// Naming Convention: [COMMAND_NAME]CommandError
-pub trait CommandError: std::error::Error {
-    type Command: Command;
+pub(in crate::kernel::commands) trait TestCommandInput: Display {
+    type Command: TestCommand;
 }
 
-// Type: Enum
-// Naming Convention: [COMMAND_NAME]CommandSuccess
-pub trait CommandSuccess: std::fmt::Debug {
-    type Command: Command;
+pub(in crate::kernel::commands) trait TestCommandOutput: Display {
+    type Command: TestCommand;
 }
 
-// Type: Struct
-// Naming Convention: "[MODULE_NAME]CommandQueueGroup"
-pub trait CommandQueueGroup {
-    type CommandModule: Command;
+pub(in crate::kernel::commands) trait TestCommandError: Display {
+    type Command: TestCommand;
+}
+
+pub(in crate::kernel::commands) trait TestCommandCode: Display {
+}
+
+pub struct TestCommands {
+}
+
+impl CommandsModule for TestCommands {
+    fn module_name() -> &'static str {
+        "TestCommands"
+    }
+}
+
+impl TestCommands {
+    pub fn hello_world(input_value: i32) -> Result<HelloWorldCommandOutput, HelloWorldCommandError> {
+        let mut hello_world_command = HelloWorldCommand::initialize(HelloWorldCommandInput {
+            value: input_value,
+        });
+
+        hello_world_command.execute();
+
+        if let Some(hello_world_command_result) = hello_world_command.finalize() {
+            hello_world_command_result
+        } else {
+            panic!("Command did not execute properly!");
+        }
+    }
+}
+
+pub(in crate::kernel::commands) enum HelloWorldCommand {
+    Initialized {
+        input: HelloWorldCommandInput,
+    },
+    Executed {
+        result: Result<HelloWorldCommandOutput, HelloWorldCommandError>,
+    },
+}
+
+impl TestCommand for HelloWorldCommand {
+    type Module = TestCommands;
+    type Input = HelloWorldCommandInput;
+    type Output = HelloWorldCommandOutput;
+    type Error = HelloWorldCommandError;
+
+    fn initialize(input: Self::Input) -> Self {
+        HelloWorldCommand::Initialized {
+            input,
+        }
+    }
+
+    fn execute(&mut self) {
+        if let HelloWorldCommand::Initialized { input } = self {
+            match input.value {
+                0 => {
+                    *self = HelloWorldCommand::Executed {
+                        result: Ok(HelloWorldCommandOutput {
+                            value: input.value,
+                        }),
+                    };
+                },
+                _ => {
+                    *self = HelloWorldCommand::Executed {
+                        result: Err(HelloWorldCommandError::InvalidInput),
+                    };
+                },
+            }
+        }
+    }
+
+    fn finalize(self) -> Option<Result<Self::Output, Self::Error>> {
+        if let HelloWorldCommand::Executed { result } = self {
+            Some(result)
+        } else {
+            None
+        }
+    }
+}
+
+pub struct HelloWorldCommandInput {
+    pub value: i32,
+}
+
+impl TestCommandInput for HelloWorldCommandInput {
+    type Command = HelloWorldCommand;
+}
+
+impl Display for HelloWorldCommandInput {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), Error> {
+        write!(f, "HelloWorldCommandInput {{ value: {} }}", self.value)
+    }
+}
+
+pub struct HelloWorldCommandOutput {
+    pub value: i32,
+}
+
+impl TestCommandOutput for HelloWorldCommandOutput {
+    type Command = HelloWorldCommand;
+}
+
+impl Display for HelloWorldCommandOutput {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), Error> {
+        write!(f, "HelloWorldCommandOutput {{ value: {} }}", self.value)
+    }
+}
+
+pub enum HelloWorldCommandError {
+    InvalidInput,
+}
+
+impl TestCommandError for HelloWorldCommandError {
+    type Command = HelloWorldCommand;
+}
+
+impl Display for HelloWorldCommandError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), Error> {
+        write!(f, "HelloWorldCommandError")
+    }
+}
+
+pub struct HelloWorldCommandCode {
+    code: Fn<(Result<HelloWorldCommandOutput, HelloWorldCommandError>, HelloWorldCommandInput)>,
 }
--- spacetime_engine_derive/src/lib.rs
+++ spacetime_engine_derive/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn;
+use syn::{parse_macro_input, LitStr, Token, parse::{Parse, ParseStream}};
 
 #[proc_macro_derive(HelloMacro)]
 pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
@@ -19,4 +19,52 @@ fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
         }
     };
     gen.into()
+}
+
+// Define a custom parser for your structured input
+struct CommandModuleType {
+    module_name: LitStr,
+    command_types: Vec<CommandType>,
+}
+
+struct CommandType {
+    command_name: LitStr,
+    input_type: CommandInputType,
+    output_type: CommandOutputType,
+    error_type: CommandErrorType,
+}
+
+struct CommandInputType {
+    parameter_types: Vec<(LitStr, syn::Type)>
+}
+
+struct CommandOutputType {
+    return_type: syn::Type
+}
+
+struct CommandErrorType {
+    error_type: syn::Type
+}
+
+// Implement Parse for CommandModule
+impl Parse for CommandModuleType {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        // Implement parsing logic based on your structure
+        // This is where you'll parse the structured input
+    }
+}
+
+#[proc_macro]
+pub fn define_commands_module(input: TokenStream) -> TokenStream {
+    // Parse the input tokens into your CommandModule struct
+    let _parsed = parse_macro_input!(input as CommandModuleType);
+
+    // Here you'd generate code based on the parsed input
+
+    // For now, just an example to generate an empty impl block
+    let expanded = quote! {
+        // Generated code would go here
+    };
+
+    TokenStream::from(expanded)
 }
