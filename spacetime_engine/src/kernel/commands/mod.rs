e02ca3bc90f473e5d8e1bc8ade8daf44271625bd
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2024-03-16 11:18:25 +0100

    WIP Commands Framework

--- spacetime_engine/src/kernel/commands/mod.rs
+++ spacetime_engine/src/kernel/commands/mod.rs
@@ -13,15 +13,20 @@ define_commands_module! {
             Error {
                 InvalidInput,
             },
-            Code (input) => {
-                if input.value == 0 {
-                    println!("Hello World!");
-
-                    return Ok(HelloWorldCommandOutput {
-                        value: input.value,
-                    })
-                } else {
-                    return Err(HelloWorldCommandError::InvalidInput)
+            Code |input| -> Result<Output, Error> {
+                match input.value {
+                    0 => {
+                        *self = HelloWorldCommand::Executed {
+                            result: Ok(HelloWorldCommandOutput {
+                                value: 0,
+                            }),
+                        };
+                    },
+                    _ => {
+                        *self = HelloWorldCommand::Executed {
+                            result: Err(HelloWorldCommandError::InvalidInput),
+                        };
+                    },
                 }
             }
         },
@@ -35,168 +40,28 @@ define_commands_module! {
             },
             Error {
                 InvalidStartPoint,
-                InvalidEndPoint;
+                InvalidEndPoint,
             },
-            Code (input) => {
-                if start_point.0 == 0 && start_point.1 == 0 {
-                    if end_point.0 == 0 && end_point.1 == 0 {
-                        println!("Drawing Gizmo Line!");
+            Code |input| -> Result<Output, Error> {
+                if input.start_point.0 == 0 && input.start_point.1 == 0 {
+                    if input.end_point.0 == 0 && input.end_point.1 == 0 {
+                        println!("Pretending to draw Gizmo Line!");
 
                         return Ok(DrawGizmoLineCommandOutput {
-                            value: input.value,
+                            line_id: 0,
                         })
                     } else {
                         return Err(DrawGizmoLineCommandError::InvalidEndPoint)
                     }
                 } else {
-                    return Err(DrawGizmoLineCommandError::InvalidPoints)
+                    return Err(DrawGizmoLineCommandError::InvalidStartPoint)
                 }
             }
         }
     }
 }
 
-
-
 // This does not belong to the Test Command Module, but is a general Commands-related concept. The TestCommand Module's presence is to demonstrate the concept of a Command Module.
 pub trait CommandsModule {
     fn module_name() -> &'static str;
-}
-
-pub(in crate::kernel::commands) trait TestCommand {
-    type Module: CommandsModule;
-    type Input: TestCommandInput<Command = Self>;
-    type Output: TestCommandOutput<Command = Self>;
-    type Error: TestCommandError<Command = Self>;
-
-    fn initialize(input: Self::Input) -> Self;
-    fn execute(&mut self);
-    fn finalize(self) -> Option<Result<Self::Output, Self::Error>>;
-}
-
-pub(in crate::kernel::commands) trait TestCommandInput: Display {
-    type Command: TestCommand;
-}
-pub(in crate::kernel::commands) trait TestCommandOutput: Display {
-    type Command: TestCommand;
-}
-pub(in crate::kernel::commands) trait TestCommandError: Display {
-    type Command: TestCommand;
-}
-pub(in crate::kernel::commands) trait TestCommandCode: Display {
-}
-pub struct TestCommands {
-}
-
-impl CommandsModule for TestCommands {
-    fn module_name() -> &'static str {
-        "TestCommands"
-    }
-}
-
-impl TestCommands {
-    pub fn hello_world(input_value: i32) -> Result<HelloWorldCommandOutput, HelloWorldCommandError> {
-        let mut hello_world_command = HelloWorldCommand::initialize(HelloWorldCommandInput {
-            value: input_value,
-        });
-
-        hello_world_command.execute();
-
-        if let Some(hello_world_command_result) = hello_world_command.finalize() {
-            hello_world_command_result
-        } else {
-            panic!("Command did not execute properly!");
-        }
-    }
-}
-
-pub(in crate::kernel::commands) enum HelloWorldCommand {
-    Initialized {
-        input: HelloWorldCommandInput,
-    },
-    Executed {
-        result: Result<HelloWorldCommandOutput, HelloWorldCommandError>,
-    },
-}
-
-impl TestCommand for HelloWorldCommand {
-    type Module = TestCommands;
-    type Input = HelloWorldCommandInput;
-    type Output = HelloWorldCommandOutput;
-    type Error = HelloWorldCommandError;
-
-    fn initialize(input: Self::Input) -> Self {
-        HelloWorldCommand::Initialized {
-            input,
-        }
-    }
-
-    fn execute(&mut self) {
-        if let HelloWorldCommand::Initialized { input } = self {
-            match input.value {
-                0 => {
-                    *self = HelloWorldCommand::Executed {
-                        result: Ok(HelloWorldCommandOutput {
-                            value: input.value,
-                        }),
-                    };
-                },
-                _ => {
-                    *self = HelloWorldCommand::Executed {
-                        result: Err(HelloWorldCommandError::InvalidInput),
-                    };
-                },
-            }
-        }
-    }
-
-    fn finalize(self) -> Option<Result<Self::Output, Self::Error>> {
-        if let HelloWorldCommand::Executed { result } = self {
-            Some(result)
-        } else {
-            None
-        }
-    }
-}
-
-pub struct HelloWorldCommandInput {
-    pub value: i32,
-}
-
-impl TestCommandInput for HelloWorldCommandInput {
-    type Command = HelloWorldCommand;
-}
-
-impl Display for HelloWorldCommandInput {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), Error> {
-        write!(f, "HelloWorldCommandInput {{ value: {} }}", self.value)
-    }
-}
-
-pub struct HelloWorldCommandOutput {
-    pub value: i32,
-}
-
-impl TestCommandOutput for HelloWorldCommandOutput {
-    type Command = HelloWorldCommand;
-}
-
-impl Display for HelloWorldCommandOutput {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), Error> {
-        write!(f, "HelloWorldCommandOutput {{ value: {} }}", self.value)
-    }
-}
-
-pub enum HelloWorldCommandError {
-    InvalidInput,
-}
-
-impl TestCommandError for HelloWorldCommandError {
-    type Command = HelloWorldCommand;
-}
-
-impl Display for HelloWorldCommandError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), Error> {
-        write!(f, "HelloWorldCommandError")
-    }
 }
--- spacetime_engine_derive/src/lib.rs
+++ spacetime_engine_derive/src/lib.rs
@@ -1,11 +1,14 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
-    punctuated::Punctuated,
+    Block,
+    ExprClosure,
+    Ident,
     LitStr, 
-    token::Comma, 
-    Token,
     parse::{Parse, ParseStream}, 
+    punctuated::Punctuated,
+    spanned::Spanned,
+    Token,
 };
 
 #[proc_macro_derive(HelloMacro)]
@@ -34,7 +37,9 @@ struct CommandModuleType {
 
 impl Parse for CommandModuleType {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let module_name = input.parse::<LitStr>()?;
+        let module_name = input.parse::<Ident>()?;
+        let module_name = module_name.to_string();
+        let module_name = LitStr::new(&module_name, module_name.span());
 
         let content;
         syn::braced!(content in input);
@@ -54,11 +59,14 @@ struct CommandType {
     input_type: CommandInputType,
     output_type: CommandOutputType,
     error_type: CommandErrorType,
+    code_type: CommandCodeType
 }
 
 impl Parse for CommandType {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let command_name = input.parse::<LitStr>()?;
+        let command_name = input.parse::<Ident>()?;
+        let command_name = command_name.to_string();
+        let command_name = LitStr::new(&command_name, command_name.span());
 
         let content;
         syn::braced!(content in input);
@@ -73,11 +81,16 @@ impl Parse for CommandType {
 
         let error_type = content.parse::<CommandErrorType>()?;
 
+        content.parse::<Token![,]>()?;
+
+        let code_type = content.parse::<CommandCodeType>()?;
+
         Ok(CommandType {
             command_name,
             input_type,
             output_type,
-            error_type
+            error_type,
+            code_type
         })
     }
 
@@ -89,7 +102,13 @@ struct CommandInputType {
 
 impl Parse for CommandInputType {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        input.parse::<LitStr>()?;
+        let input_label = input.parse::<Ident>()?;
+        let span = input_label.span();
+        let input_label = input_label.to_string();
+
+        if input_label != "Input" {
+            return Err(syn::Error::new(span, "Expected 'Input' Label"));
+        }
 
         let content;
         syn::braced!(content in input);
@@ -109,7 +128,9 @@ struct CommandInputParameterType {
 
 impl Parse for CommandInputParameterType {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let parameter_name = input.parse()?;
+        let parameter_label = input.parse::<Ident>()?;
+        let parameter_name = parameter_label.to_string();
+        let parameter_name = LitStr::new(&parameter_name, parameter_name.span());
 
         input.parse::<Token![:]>()?;
 
@@ -128,7 +149,13 @@ struct CommandOutputType {
 
 impl Parse for CommandOutputType {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        input.parse::<LitStr>()?;
+        let output_label = input.parse::<Ident>()?;
+        let span = output_label.span();
+        let output_label = output_label.to_string();
+
+        if output_label != "Output" {
+            return Err(syn::Error::new(span, "Expected 'Output' Label"));
+        }
 
         let content;
         syn::braced!(content in input);
@@ -148,7 +175,9 @@ struct CommandOutputParameterType {
 
 impl Parse for CommandOutputParameterType {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let parameter_name = input.parse()?;
+        let parameter_label = input.parse::<Ident>()?;
+        let parameter_name = parameter_label.to_string();
+        let parameter_name = LitStr::new(&parameter_name, parameter_name.span());
 
         input.parse::<Token![:]>()?;
 
@@ -168,7 +197,13 @@ struct CommandErrorType {
 
 impl Parse for CommandErrorType {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        input.parse::<LitStr>()?;
+        let error_label = input.parse::<Ident>()?;
+        let span = error_label.span();
+        let error_label = error_label.to_string();
+
+        if error_label != "Error" {
+            return Err(syn::Error::new(span, "Expected 'Error' Label"));
+        }
 
         let content;
         syn::braced!(content in input);
@@ -187,7 +222,9 @@ struct CommandErrorVariantType {
 
 impl Parse for CommandErrorVariantType {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let variant_name = input.parse()?;
+        let variant_label = input.parse::<Ident>()?;
+        let variant_name = variant_label.to_string();
+        let variant_name = LitStr::new(&variant_name, variant_name.span());
 
         Ok(CommandErrorVariantType {
             variant_name
@@ -195,12 +232,246 @@ impl Parse for CommandErrorVariantType {
     }
 }
 
+struct CommandCodeType {
+    code_block: Block
+}
+
+impl Parse for CommandCodeType {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let code_label = input.parse::<Ident>()?;
+        let span = code_label.span();
+        let code_label = code_label.to_string();
+
+        if code_label != "Code" {
+            return Err(syn::Error::new(span, "Expected 'Code' Label"));
+        }
+
+        input.parse::<Token![|]>()?;
+
+        let input_parameter_label = input.parse::<Ident>()?;
+        let span = input_parameter_label.span();
+        let input_parameter_label = input_parameter_label.to_string();
+
+        if input_parameter_label != "input" {
+            return Err(syn::Error::new(span, "Expected 'input' Label"));
+        }
+
+        input.parse::<Token![|]>()?;
+
+        input.parse::<Token![-]>()?;
+        input.parse::<Token![>]>()?;
+
+        let result_paramater_label = input.parse::<Ident>()?;
+        let span = result_paramater_label.span();
+        let result_paramater_label = result_paramater_label.to_string();
+
+        if result_paramater_label != "Result" {
+            return Err(syn::Error::new(span, "Expected 'Result' Label"));
+        }
+
+        input.parse::<Token![<]>()?;
+
+        let output_parameter_label = input.parse::<Ident>()?;
+        let span = output_parameter_label.span();
+        let output_parameter_label = output_parameter_label.to_string();
+
+        if output_parameter_label != "Output" {
+            return Err(syn::Error::new(span, "Expected 'Output' Label"));
+        }
+
+        input.parse::<Token![,]>()?;
+
+        let error_parameter_label = input.parse::<Ident>()?;
+        let span = error_parameter_label.span();
+        let error_parameter_label = error_parameter_label.to_string();
+
+        if error_parameter_label != "Error" {
+            return Err(syn::Error::new(span, "Expected 'Error' Label"));
+        }
+
+        input.parse::<Token![>]>()?;
+
+
+        let code_block = input.parse::<Block>()?;
+
+        Ok(CommandCodeType {
+            code_block
+        })
+    }
+
+}
+
 #[proc_macro]
 pub fn define_commands_module(tokens: TokenStream) -> TokenStream {
     let parsed_module = syn::parse_macro_input!(tokens as CommandModuleType);
 
     let generated_code = quote! {
-        // Generated code will go here
+        pub(in crate::kernel::commands) trait TestCommand {
+            type Module: CommandsModule;
+            type Input: TestCommandInput<Command = Self>;
+            type Output: TestCommandOutput<Command = Self>;
+            type Error: TestCommandError<Command = Self>;
+            type Code: TestCommandCode<Command = Self>;
+        
+            fn initialize(input: Self::Input, code: Self::Code) -> Self;
+            fn execute(&mut self);
+            fn finalize(self) -> Option<Result<Self::Output, Self::Error>>;
+        }
+        
+        pub(in crate::kernel::commands) trait TestCommandInput: Display {
+            type Command: TestCommand;
+        }
+        
+        pub(in crate::kernel::commands) trait TestCommandOutput: Display {
+            type Command: TestCommand;
+        }
+        
+        pub(in crate::kernel::commands) trait TestCommandError: Display {
+            type Command: TestCommand;
+        }
+        
+        pub(in crate::kernel::commands) trait TestCommandCode: Display {
+            type Command: TestCommand;
+        }
+        
+        pub struct TestCommands {
+        }
+        
+        impl CommandsModule for TestCommands {
+            fn module_name() -> &'static str {
+                "TestCommands"
+            }
+        }
+        
+        impl TestCommands {
+            pub fn hello_world(value: i32) -> Result<HelloWorldCommandOutput, HelloWorldCommandError> {
+                let mut hello_world_command = HelloWorldCommand::initialize(
+                    HelloWorldCommandInput {
+                        value,
+                    },
+                    HelloWorldCommandCode {
+                        closure: |input| -> Result<HelloWorldCommandOutput, HelloWorldCommandError> {
+                            if input.value == 0 {
+                                Ok(HelloWorldCommandOutput {
+                                    value: input.value,
+                                })
+                            } else {
+                                Err(HelloWorldCommandError::InvalidInput)
+                            }
+                        },
+                    }
+                );
+        
+                hello_world_command.execute();
+        
+                if let Some(hello_world_command_result) = hello_world_command.finalize() {
+                    hello_world_command_result
+                } else {
+                    panic!("Command did not execute properly!");
+                }
+            }
+        }
+        
+        pub(in crate::kernel::commands) enum HelloWorldCommand {
+            Initialized {
+                input: HelloWorldCommandInput,
+                code: HelloWorldCommandCode,
+            },
+            Executed {
+                result: Result<HelloWorldCommandOutput, HelloWorldCommandError>,
+            },
+        }
+        
+        impl TestCommand for HelloWorldCommand {
+            type Module = TestCommands;
+            type Input = HelloWorldCommandInput;
+            type Output = HelloWorldCommandOutput;
+            type Error = HelloWorldCommandError;
+            type Code = HelloWorldCommandCode;
+        
+            fn initialize(input: Self::Input, code: Self::Code) -> Self {
+                HelloWorldCommand::Initialized {
+                    input,
+                    code,
+                }
+            }
+        
+            fn execute(&mut self) {
+                if let HelloWorldCommand::Initialized { input, code } = self {
+                    *self = HelloWorldCommand::Executed {
+                        result: (code.closure)(input),
+                    };
+                }
+            }
+        
+            fn finalize(self) -> Option<Result<Self::Output, Self::Error>> {
+                if let HelloWorldCommand::Executed { result } = self {
+                    Some(result)
+                } else {
+                    None
+                }
+            }
+        }
+        
+        pub(in crate::kernel::commands) struct HelloWorldCommandInput {
+            pub value: i32,
+        }
+        
+        impl TestCommandInput for HelloWorldCommandInput {
+            type Command = HelloWorldCommand;
+        }
+        
+        impl Display for HelloWorldCommandInput {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), Error> {
+                write!(f, "HelloWorldCommandInput {{ value: {} }}", self.value)
+            }
+        }
+        
+        pub struct HelloWorldCommandOutput {
+            pub value: i32,
+        }
+        
+        impl TestCommandOutput for HelloWorldCommandOutput {
+            type Command = HelloWorldCommand;
+        }
+        
+        impl Display for HelloWorldCommandOutput {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), Error> {
+                write!(f, "HelloWorldCommandOutput {{ value: {} }}", self.value)
+            }
+        }
+        
+        pub enum HelloWorldCommandError {
+            InvalidInput,
+        }
+        
+        impl TestCommandError for HelloWorldCommandError {
+            type Command = HelloWorldCommand;
+        }
+        
+        impl Display for HelloWorldCommandError {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), Error> {
+                match *self {
+                    HelloWorldCommandError::InvalidInput => {
+                        return write!(f, "HelloWorldCommandError::InvalidInput");
+                    },
+                }
+            }
+        }
+        
+        pub(in crate::kernel::commands) struct HelloWorldCommandCode {
+            closure: fn(&HelloWorldCommandInput) -> Result<HelloWorldCommandOutput, HelloWorldCommandError>,
+        }
+        
+        impl TestCommandCode for HelloWorldCommandCode {
+            type Command = HelloWorldCommand;
+        }
+        
+        impl Display for HelloWorldCommandCode {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), Error> {
+                write!(f, "HelloWorldCommandCode {{ closure: {:?} }}", self.closure)
+            }
+        }
     };
 
     TokenStream::from(generated_code)
