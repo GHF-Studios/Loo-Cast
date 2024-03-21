022dce123cd0088528630fec59878a261d1b64ac
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2024-03-21 23:13:23 +0100

    WIP Something

--- spacetime_engine/src/kernel/assets/mod.rs
+++ spacetime_engine/src/kernel/assets/mod.rs
@@ -1,142 +1,142 @@
-use spacetime_engine_derive::{define_commands_module, define_assets_module};
-use serde::{Deserialize, Serialize};
-
-pub struct TestAssets {}
-
-impl TestAssets {
-    pub fn load_file(
-        file_path: std::path::PathBuf,
-        data_id: u64,
-        file_registry: &FileRegistry,
-        data_registry: &DataRegistry,
-    ) -> Result<(), LoadFileCommandError> {
-        let input = LoadFileCommandInput {
-            file_path,
-            data_id,
-            file_registry,
-            data_registry,
-        };
-
-        match LoadFileCommand::execute(input) {
-            Ok(_) => {
-                Ok(())
-            },
-            Err(error) => {
-                Err(error)
-            },
-        }
-    }
-
-}
-
-pub struct PlayerStatsAsset {
-    file_path: PathBuf,
-    data_id: u64,
-}
-
-impl PlayerStatsAsset {
-    pub fn new(file_path: PathBuf, data_id: u64) -> Self {
-        Self {
-            file_path,
-            data_id,
-        }
-    }
-
-    pub fn deserialize_asset(
-        &self, 
-        file_registry: &mut TestFilesRegistry, 
-        data_registry: &mut TestDatasRegistry
-    ) -> Result<PlayerStatsData, PlayerStatsFileDeserializeError> {
-    }
-
-    pub fn serialize_asset(
-        &self,
-        file_registry: &mut TestFilesRegistry,
-        data_registry: &mut TestDatasRegistry,
-    ) -> Result<PlayerStatsFile, PlayerStatsDataSerializeError> {
-    }
-}
-
-impl Asset for PlayerStatsAsset {
-    
-}
-
-pub struct PlayerStatsFile(crate::kernel::files::BinaryFile);
-
-#[derive(Serialize, Deserialize)]
-pub struct PlayerStatsData {
-    pub id: u64,
-    pub name: String,
-    pub high_score: u32,
-}
-
-pub enum PlayerStatsFileDeserializeError {
-    FileReadError,
-    DeserializeError,
-    DataWriteError,
-}
-
-pub enum PlayerStatsDataSerializeError {
-    DataReadError,
-    SerializeError,
-    FileWriteError,
-}
-
-define_assets_module! {
-    Test {
-        module_path: crate::kernel::commands,
-        assets: [
-            PlayerStats {
-                File {
-                    type_path: crate::kernel::files::Binary
-                },
-                Data {
-                    name: String,
-                    high_score: u32,
-                },
-            },
-        ]
-    }
-}
-
-define_commands_module! {
-    Asset {
-        module_path: crate::kernel::assets,
-        commands: [
-            DeserializeFile {
-                Input {
-                    file_path: std::path::PathBuf,
-                    data_id: u64,
-                },
-                Output {
-                },
-                Error {
-                    FileReadError,
-                    DeserializeError,
-                    DataWriteError,
-                },
-                Code |file_path, data_id| -> Result<Output, Error> {
-                    let file_handle = std::fs::File::open(file_path).map_err(|error| Error::FileReadError)?;
-                    let file = BinaryFile {
-                        file_handle,
-                    };
-                }
-            },
-            SerializeData {
-                Input {
-                    file_path: std::path::PathBuf,
-                    data_id: u64,
-                },
-                Output {
-                },
-                Error {
-                    DataReadError,
-                    SerializeError,
-                    FileWriteError,
-                },
-                Code |file_path, data_id| -> Result<Output, Error> {
-                }
-            }
-        ]
-    }
-}
+//use spacetime_engine_derive::{define_commands_module, define_assets_module};
+//use serde::{Deserialize, Serialize};
+//
+//pub struct TestAssets {}
+//
+//impl TestAssets {
+//    pub fn load_file(
+//        file_path: std::path::PathBuf,
+//        data_id: u64,
+//        file_registry: &FileRegistry,
+//        data_registry: &DataRegistry,
+//    ) -> Result<(), LoadFileCommandError> {
+//        let input = LoadFileCommandInput {
+//            file_path,
+//            data_id,
+//            file_registry,
+//            data_registry,
+//        };
+//
+//        match LoadFileCommand::execute(input) {
+//            Ok(_) => {
+//                Ok(())
+//            },
+//            Err(error) => {
+//                Err(error)
+//            },
+//        }
+//    }
+//
+//}
+//
+//pub struct PlayerStatsAsset {
+//    file_path: PathBuf,
+//    data_id: u64,
+//}
+//
+//impl PlayerStatsAsset {
+//    pub fn new(file_path: PathBuf, data_id: u64) -> Self {
+//        Self {
+//            file_path,
+//            data_id,
+//        }
+//    }
+//
+//    pub fn deserialize_asset(
+//        &self, 
+//        file_registry: &mut TestFilesRegistry, 
+//        data_registry: &mut TestDatasRegistry
+//    ) -> Result<PlayerStatsData, PlayerStatsFileDeserializeError> {
+//    }
+//
+//    pub fn serialize_asset(
+//        &self,
+//        file_registry: &mut TestFilesRegistry,
+//        data_registry: &mut TestDatasRegistry,
+//    ) -> Result<PlayerStatsFile, PlayerStatsDataSerializeError> {
+//    }
+//}
+//
+//impl Asset for PlayerStatsAsset {
+//    
+//}
+//
+//pub struct PlayerStatsFile(crate::kernel::files::BinaryFile);
+//
+//#[derive(Serialize, Deserialize)]
+//pub struct PlayerStatsData {
+//    pub id: u64,
+//    pub name: String,
+//    pub high_score: u32,
+//}
+//
+//pub enum PlayerStatsFileDeserializeError {
+//    FileReadError,
+//    DeserializeError,
+//    DataWriteError,
+//}
+//
+//pub enum PlayerStatsDataSerializeError {
+//    DataReadError,
+//    SerializeError,
+//    FileWriteError,
+//}
+//
+//define_assets_module! {
+//    Test {
+//        module_path: crate::kernel::commands,
+//        assets: [
+//            PlayerStats {
+//                File {
+//                    type_path: crate::kernel::files::Binary
+//                },
+//                Data {
+//                    name: String,
+//                    high_score: u32,
+//                },
+//            },
+//        ]
+//    }
+//}
+//
+//define_commands_module! {
+//    Asset {
+//        module_path: crate::kernel::assets,
+//        commands: [
+//            DeserializeFile {
+//                Input {
+//                    file_path: std::path::PathBuf,
+//                    data_id: u64,
+//                },
+//                Output {
+//                },
+//                Error {
+//                    FileReadError,
+//                    DeserializeError,
+//                    DataWriteError,
+//                },
+//                Code |file_path, data_id| -> Result<Output, Error> {
+//                    let file_handle = std::fs::File::open(file_path).map_err(|error| Error::FileReadError)?;
+//                    let file = BinaryFile {
+//                        file_handle,
+//                    };
+//                }
+//            },
+//            SerializeData {
+//                Input {
+//                    file_path: std::path::PathBuf,
+//                    data_id: u64,
+//                },
+//                Output {
+//                },
+//                Error {
+//                    DataReadError,
+//                    SerializeError,
+//                    FileWriteError,
+//                },
+//                Code |file_path, data_id| -> Result<Output, Error> {
+//                }
+//            }
+//        ]
+//    }
+//}
--- spacetime_engine/src/kernel/files/mod.rs
+++ spacetime_engine/src/kernel/files/mod.rs
@@ -1,21 +1,21 @@
-use spacetime_engine_derive::define_files_module;
-
-pub struct BinaryFile {
-    pub file_handle: std::fs::File,
-}
-
-impl File for BinaryFile {
-    
-}
-
-define_files_module! {
-    Test {
-        module_path: crate::kernel::files,
-        files: [
-            Binary(Vec<u8>),
-            Plaintext(String),
-            XML(String),
-            JSON(String),
-        ]
-    }
-}
+//use spacetime_engine_derive::define_files_module;
+//
+//pub struct BinaryFile {
+//    pub file_handle: std::fs::File,
+//}
+//
+//impl File for BinaryFile {
+//    
+//}
+//
+//define_files_module! {
+//    Test {
+//        module_path: crate::kernel::files,
+//        files: [
+//            Binary(Vec<u8>),
+//            Plaintext(String),
+//            XML(String),
+//            JSON(String),
+//        ]
+//    }
+//}
