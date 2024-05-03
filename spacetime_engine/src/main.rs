dfa37831e09cfbfb2b4c2586c28d899c24f757b8
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2024-05-03 23:54:27 +0200

    WIP Prototype Mk2

--- spacetime_engine/src/main.rs
+++ spacetime_engine/src/main.rs
@@ -1,28 +1,179 @@
 use std::collections::HashMap;
-
+use std::ops;
 use bevy::{math::I16Vec2, prelude::*};
 use bevy_rapier2d::prelude::*;
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+struct ChunkCoordinate(I16Vec2);
+
+impl From<I16Vec2> for ChunkCoordinate {
+    fn from(i16_vec2: I16Vec2) -> Self {
+        ChunkCoordinate(i16_vec2)
+    }
+}
+
+impl Into<I16Vec2> for ChunkCoordinate {
+    fn into(self) -> I16Vec2 {
+        self.0
+    }
+}
+
+impl From<EntityCoordinate> for ChunkCoordinate {
+    fn from(entity_coordinate: EntityCoordinate) -> Self {
+        let x = (entity_coordinate.0.x / CHUNK_SIZE as f32).floor() as i16;
+        let y = (entity_coordinate.0.y / CHUNK_SIZE as f32).floor() as i16;
+        ChunkCoordinate(I16Vec2::new(x, y))
+    }
+}
+
+impl ops::Add<ChunkCoordinate> for ChunkCoordinate {
+    type Output = ChunkCoordinate;
+
+    fn add(self, other: ChunkCoordinate) -> ChunkCoordinate {
+        ChunkCoordinate(self.0 + other.0)
+    }
+}
+
+impl ops::Sub<ChunkCoordinate> for ChunkCoordinate {
+    type Output = ChunkCoordinate;
+
+    fn sub(self, other: ChunkCoordinate) -> ChunkCoordinate {
+        ChunkCoordinate(self.0 - other.0)
+    }
+}
+
+impl ops::Mul<i16> for ChunkCoordinate {
+    type Output = ChunkCoordinate;
+
+    fn mul(self, scalar: i16) -> ChunkCoordinate {
+        ChunkCoordinate(self.0 * scalar)
+    }
+}
+
+impl ops::Div<i16> for ChunkCoordinate {
+    type Output = ChunkCoordinate;
+
+    fn div(self, scalar: i16) -> ChunkCoordinate {
+        ChunkCoordinate(self.0 / scalar)
+    }
+}
+
+impl ChunkCoordinate {
+    fn new(x: i16, y: i16) -> Self {
+        ChunkCoordinate(I16Vec2::new(x, y))
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+struct ChunkID(ChunkCoordinate);
+
+impl From<ChunkCoordinate> for ChunkID {
+    fn from(chunk_coordinate: ChunkCoordinate) -> Self {
+        ChunkID(chunk_coordinate)
+    }
+}
+
+impl Into<ChunkCoordinate> for ChunkID {
+    fn into(self) -> ChunkCoordinate {
+        self.0
+    }
+}
+
+impl ChunkID {
+    fn new(x: i16, y: i16) -> Self {
+        ChunkID(ChunkCoordinate::new(x, y))
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct EntityCoordinate(Vec3);
+
+impl From<Vec3> for EntityCoordinate {
+    fn from(vec3: Vec3) -> Self {
+        EntityCoordinate(vec3)
+    }
+}
+
+impl Into<Vec3> for EntityCoordinate {
+    fn into(self) -> Vec3 {
+        self.0
+    }
+}
+
+impl From<ChunkCoordinate> for EntityCoordinate {
+    fn from(chunk_coordinate: ChunkCoordinate) -> Self {
+        let x = chunk_coordinate.0.x as f32 * CHUNK_SIZE as f32;
+        let y = chunk_coordinate.0.y as f32 * CHUNK_SIZE as f32;
+        EntityCoordinate(Vec3::new(x, y, CHUNK_Z_INDEX))
+    }
+}
+
+impl ops::Add<EntityCoordinate> for EntityCoordinate {
+    type Output = EntityCoordinate;
+
+    fn add(self, other: EntityCoordinate) -> EntityCoordinate {
+        EntityCoordinate(self.0 + other.0)
+    }
+}
+
+impl ops::Sub<EntityCoordinate> for EntityCoordinate {
+    type Output = EntityCoordinate;
+
+    fn sub(self, other: EntityCoordinate) -> EntityCoordinate {
+        EntityCoordinate(self.0 - other.0)
+    }
+}
+
+impl ops::Mul<f32> for EntityCoordinate {
+    type Output = EntityCoordinate;
+
+    fn mul(self, scalar: f32) -> EntityCoordinate {
+        EntityCoordinate(self.0 * scalar)
+    }
+}
+
+impl ops::Div<f32> for EntityCoordinate {
+    type Output = EntityCoordinate;
+
+    fn div(self, scalar: f32) -> EntityCoordinate {
+        EntityCoordinate(self.0 / scalar)
+    }
+}
+
+impl EntityCoordinate {
+    fn new(x: f32, y: f32) -> Self {
+        EntityCoordinate(Vec3::new(x, y, CHUNK_Z_INDEX))
+    }
+}
+
+const CHUNK_SIZE: u16 = 128;
+const CHUNK_Z_INDEX: f32 = -1.0;
+
 #[derive(Resource)]
 struct ChunkManager {
-    chunk_size: i16,
-    registered_chunks: Vec<I16Vec2>,
-    loaded_chunks: HashMap<I16Vec2, Entity>,
+    registered_chunks: Vec<ChunkID>,
+    loaded_chunks: HashMap<ChunkID, Entity>,
 }
 
 #[derive(Component)]
 struct ChunkLoader {
-    load_radius: i16,
-    current_chunk_ids: Vec<I16Vec2>,
+    load_radius: u16,
+    current_chunk_ids: Vec<ChunkID>,
 }
 
 #[derive(Component)]
 struct Chunk {
-    id: I16Vec2,  // Identifier for the chunk based on its position
+    id: ChunkID,  // Identifier for the chunk based on its position
+    tracked_entities: Vec<Entity>,  // Entities currently within this chunk
 }
 
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct TrackedEntity {
+    current_chunk: Option<ChunkID>,  // The chunk ID the entity is currently within, None if in the void
+}
 
 #[derive(Component)]
 struct Follower {
@@ -60,7 +211,7 @@ fn main_setup_system(mut commands: Commands) {
     .id();
     
     // Chunk manager
-    commands.insert_resource(ChunkManager { chunk_size: 64, registered_chunks: Vec::new(), loaded_chunks: HashMap::new() });
+    commands.insert_resource(ChunkManager { registered_chunks: Vec::new(), loaded_chunks: HashMap::new() });
 
     // Camera that follows the player
     let _camera_entity = commands.spawn(Camera2dBundle::default())
@@ -99,30 +250,31 @@ fn chunk_loader_system(
     mut chunk_manager: ResMut<ChunkManager>,
 ) {
     let (chunk_loader_transform, mut chunk_loader) = chunk_loader_query.single_mut();
-    let chunk_loader_translation = chunk_loader_transform.translation.truncate();
-    let chunk_size = chunk_manager.chunk_size as f32;
-    let current_chunk_id = chunk_loader_translation / chunk_size;
-    let current_chunk_id = I16Vec2::new(current_chunk_id.x.floor() as i16, current_chunk_id.y.floor() as i16);
-    let load_radius = chunk_loader.load_radius;
+    let chunk_loader_entity_coordinate: EntityCoordinate = chunk_loader_transform.translation.into();
+    let current_chunk_coordinate: ChunkCoordinate = chunk_loader_entity_coordinate.into();
+    let load_radius = chunk_loader.load_radius as i16;
     
     // Detect chunks around the player
-    let mut detected_chunk_ids: Vec<I16Vec2> = Vec::new();
+    let mut detected_chunk_coordinates = Vec::new();
     for x_offset in -load_radius..=load_radius {
         for y_offset in -load_radius..=load_radius {
-            detected_chunk_ids.push(current_chunk_id + I16Vec2::new(x_offset, y_offset));
+            detected_chunk_coordinates.push(current_chunk_coordinate + ChunkCoordinate::new(x_offset, y_offset));
         }
     }
 
     // Categorize the detected chunks
-    let mut old_chunk_ids: Vec<I16Vec2> = Vec::new(); // Chunks which are active, but have not been detected
-    let mut unchanged_chunk_ids: Vec<I16Vec2> = Vec::new(); // Chunks which are active and have been detected
-    let mut new_chunk_ids: Vec<I16Vec2> = Vec::new(); // Chunks which are not active but have been detected
+    let mut old_chunk_ids: Vec<ChunkID> = Vec::new(); // Chunks which are active, but have not been detected
+    let mut unchanged_chunk_ids: Vec<ChunkID> = Vec::new(); // Chunks which are active and have been detected
+    let mut new_chunk_ids: Vec<ChunkID> = Vec::new(); // Chunks which are not active but have been detected
     for (loaded_chunk_id, _) in chunk_manager.loaded_chunks.iter() {
-        if !detected_chunk_ids.contains(loaded_chunk_id) {
+        let loaded_chunk_coordinate: ChunkCoordinate = loaded_chunk_id.clone().into();
+
+        if !detected_chunk_coordinates.contains(&loaded_chunk_coordinate) {
             old_chunk_ids.push(*loaded_chunk_id);
         }
     }
-    for detected_chunk_id in detected_chunk_ids {
+    for detected_chunk_coordinate in detected_chunk_coordinates {
+        let detected_chunk_id: ChunkID = detected_chunk_coordinate.clone().into();
         if chunk_manager.loaded_chunks.contains_key(&detected_chunk_id) {
             unchanged_chunk_ids.push(detected_chunk_id);
         } else {
@@ -133,7 +285,7 @@ fn chunk_loader_system(
     // Handle old chunks
     for old_chunk_id in old_chunk_ids {
         // "Unload" the chunk
-        // TODO: Implement actual chunk unloading
+        // TODO: Implement actual chunk unloading and entity serialization
         if let Some(loaded_chunk_entity) = chunk_manager.loaded_chunks.remove(&old_chunk_id) {
             commands.entity(loaded_chunk_entity).despawn_recursive();
         }
@@ -143,13 +295,13 @@ fn chunk_loader_system(
     for new_chunk_id in new_chunk_ids.clone() {
         if chunk_manager.registered_chunks.contains(&new_chunk_id) {
             // "Load" the chunk
-            // TODO: Implement actual chunk loading
-            let new_chunk_entity = new_chunk_entity(&mut commands, &mut chunk_manager, new_chunk_id);
+            // TODO: Implement actual chunk loading and entity deserialization
+            let new_chunk_entity = new_chunk_entity(&mut commands, new_chunk_id);
 
             chunk_manager.loaded_chunks.insert(new_chunk_id, new_chunk_entity);
         } else {
             // Create a new chunk
-            let new_chunk_entity = new_chunk_entity(&mut commands, &mut chunk_manager, new_chunk_id);
+            let new_chunk_entity = new_chunk_entity(&mut commands, new_chunk_id);
 
             chunk_manager.registered_chunks.push(new_chunk_id);
             chunk_manager.loaded_chunks.insert(new_chunk_id, new_chunk_entity);
@@ -161,33 +313,43 @@ fn chunk_loader_system(
     chunk_loader.current_chunk_ids.append(&mut new_chunk_ids);
 }
 
-fn new_chunk_entity(commands: &mut Commands, chunk_manager: &mut ResMut<ChunkManager>, chunk_id: I16Vec2) -> Entity {
-    let chunk_size = chunk_manager.chunk_size as f32;
-    let chunk_translation = Vec3::new(
-        (chunk_id.x as f32 * chunk_size) + chunk_size / 2.0, 
-        (chunk_id.y as f32 * chunk_size) + chunk_size / 2.0, 
-        -1.0);
+fn new_chunk_entity(commands: &mut Commands, chunk_id: ChunkID) -> Entity {
+    let chunk_coordinate: ChunkCoordinate = chunk_id.into();
+    let chunk_entity_coordinate: EntityCoordinate = chunk_coordinate.into();
 
-    let chunk_color = if (chunk_id.x + chunk_id.y) % 2 == 0 {
+    let chunk_color = if (chunk_coordinate.0.x + chunk_coordinate.0.y) % 2 == 0 {
         Color::rgb(0.25, 0.25, 0.25)
     } else {
         Color::rgb(0.75, 0.75, 0.75)
     };
 
     let chunk_entity = commands.spawn((
-        Chunk { id: chunk_id },
+        Chunk { id: chunk_id, tracked_entities: Vec::new()},
         SpriteBundle {
             sprite: Sprite {
                 color: chunk_color,
-                custom_size: Some(Vec2::new(chunk_size, chunk_size)),
+                custom_size: Some(Vec2::new(CHUNK_SIZE as f32, CHUNK_SIZE as f32)),
                 ..default()
             },
-            transform: Transform::from_translation(chunk_translation),
+            transform: Transform::from_translation(chunk_entity_coordinate.0),
             ..default()
         },
     )).id();
 
     chunk_entity
+}
+
+fn tracked_entity_system(
+    mut commands: Commands,
+    mut tracked_entity_query: Query<(Entity, &mut TrackedEntity, &Transform)>,
+    mut chunk_query: Query<(&mut Chunk)>,
+    chunk_manager: Res<ChunkManager>,
+) {
+    // If a tracked entity has no current chunk and no chunk is found, do nothing
+    // If a tracked entity has no current chunk and a chunk is found, add the entity to the chunk, and set the current chunk
+    // If a tracked entity has a current chunk and that same chunk is found, do nothing
+    // If a tracked entity has a current chunk and a different chunk is found, remove the entity from the current chunk, add it to the new chunk, and update the current chunk
+    // If a tracked entity has a current chunk and no chunk is found, remove the entity from the current chunk and destroy the entity recursively
 }
 
 fn follower_system(
