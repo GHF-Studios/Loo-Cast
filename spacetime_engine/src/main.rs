d85a61ec5926b7e1db8d1512caddc798bc0bc584
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2024-05-04 18:21:54 +0200

    Prototype Mk2

--- spacetime_engine/src/main.rs
+++ spacetime_engine/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::ops;
-use bevy::{math::I16Vec2, prelude::*};
+use bevy::{math::I16Vec2, prelude::*, window::PrimaryWindow};
 use bevy_rapier2d::prelude::*;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
@@ -18,10 +18,10 @@ impl Into<I16Vec2> for ChunkCoordinate {
     }
 }
 
-impl From<EntityCoordinate> for ChunkCoordinate {
-    fn from(entity_coordinate: EntityCoordinate) -> Self {
-        let x = (entity_coordinate.0.x / CHUNK_SIZE as f32).floor() as i16;
-        let y = (entity_coordinate.0.y / CHUNK_SIZE as f32).floor() as i16;
+impl From<ObjectCoordinate> for ChunkCoordinate {
+    fn from(object_coordinate: ObjectCoordinate) -> Self {
+        let x = (object_coordinate.0.x / CHUNK_SIZE as f32).floor() as i16;
+        let y = (object_coordinate.0.y / CHUNK_SIZE as f32).floor() as i16;
         ChunkCoordinate(I16Vec2::new(x, y))
     }
 }
@@ -86,95 +86,110 @@ impl ChunkID {
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-struct EntityCoordinate(Vec3);
+struct ObjectCoordinate(Vec3);
 
-impl From<Vec3> for EntityCoordinate {
-    fn from(vec3: Vec3) -> Self {
-        EntityCoordinate(vec3)
+impl From<Vec2> for ObjectCoordinate {
+    fn from(vec2: Vec2) -> Self {
+        ObjectCoordinate(Vec3::new(vec2.x, vec2.y, 0.0))
     }
 }
 
-impl Into<Vec3> for EntityCoordinate {
+impl From<Vec3> for ObjectCoordinate {
+    fn from(vec3: Vec3) -> Self {
+        ObjectCoordinate(vec3)
+    }
+}
+
+impl Into<Vec3> for ObjectCoordinate {
     fn into(self) -> Vec3 {
         self.0
     }
 }
 
-impl From<ChunkCoordinate> for EntityCoordinate {
+impl From<ChunkCoordinate> for ObjectCoordinate {
     fn from(chunk_coordinate: ChunkCoordinate) -> Self {
         let x = chunk_coordinate.0.x as f32 * CHUNK_SIZE as f32;
         let y = chunk_coordinate.0.y as f32 * CHUNK_SIZE as f32;
-        EntityCoordinate(Vec3::new(x, y, CHUNK_Z_INDEX))
+        ObjectCoordinate(Vec3::new(x, y, CHUNK_Z_INDEX))
     }
 }
 
-impl ops::Add<EntityCoordinate> for EntityCoordinate {
-    type Output = EntityCoordinate;
+impl ops::Add<ObjectCoordinate> for ObjectCoordinate {
+    type Output = ObjectCoordinate;
 
-    fn add(self, other: EntityCoordinate) -> EntityCoordinate {
-        EntityCoordinate(self.0 + other.0)
+    fn add(self, other: ObjectCoordinate) -> ObjectCoordinate {
+        ObjectCoordinate(self.0 + other.0)
     }
 }
 
-impl ops::Sub<EntityCoordinate> for EntityCoordinate {
-    type Output = EntityCoordinate;
+impl ops::Sub<ObjectCoordinate> for ObjectCoordinate {
+    type Output = ObjectCoordinate;
 
-    fn sub(self, other: EntityCoordinate) -> EntityCoordinate {
-        EntityCoordinate(self.0 - other.0)
+    fn sub(self, other: ObjectCoordinate) -> ObjectCoordinate {
+        ObjectCoordinate(self.0 - other.0)
     }
 }
 
-impl ops::Mul<f32> for EntityCoordinate {
-    type Output = EntityCoordinate;
+impl ops::Mul<f32> for ObjectCoordinate {
+    type Output = ObjectCoordinate;
 
-    fn mul(self, scalar: f32) -> EntityCoordinate {
-        EntityCoordinate(self.0 * scalar)
+    fn mul(self, scalar: f32) -> ObjectCoordinate {
+        ObjectCoordinate(self.0 * scalar)
     }
 }
 
-impl ops::Div<f32> for EntityCoordinate {
-    type Output = EntityCoordinate;
+impl ops::Div<f32> for ObjectCoordinate {
+    type Output = ObjectCoordinate;
 
-    fn div(self, scalar: f32) -> EntityCoordinate {
-        EntityCoordinate(self.0 / scalar)
+    fn div(self, scalar: f32) -> ObjectCoordinate {
+        ObjectCoordinate(self.0 / scalar)
     }
 }
 
-impl EntityCoordinate {
+impl ObjectCoordinate {
     fn new(x: f32, y: f32) -> Self {
-        EntityCoordinate(Vec3::new(x, y, CHUNK_Z_INDEX))
+        ObjectCoordinate(Vec3::new(x, y, CHUNK_Z_INDEX))
     }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-struct EntityID(u64);
+struct ChunkActorID(u64);
 
 const CHUNK_SIZE: u16 = 128;
 const CHUNK_Z_INDEX: f32 = -1.0;
 
+const PLAYER_MOVEMENT_SPEED: f32 = 1000.0;
+const PLAYER_CREATIVE_SQUARE_PROP_SIZE: f32 = 50.0;
+
 #[derive(Resource)]
-struct UniverseManager {
+struct ChunkManager {
     registered_chunks: Vec<ChunkID>,
     loaded_chunks: HashMap<ChunkID, Entity>,
-    current_entity_id: EntityID,
-    recycled_entity_ids: Vec<EntityID>,
+    current_chunk_actor_id: ChunkActorID,
+    recycled_chunk_actor_ids: Vec<ChunkActorID>,
 }
 
-impl UniverseManager {
-    fn get_unused_entity_id(&mut self) -> EntityID {
-        if let Some(recycled_entity_id) = self.recycled_entity_ids.pop() {
-            recycled_entity_id
+impl ChunkManager {
+    fn get_unused_chunk_actor_id(&mut self) -> ChunkActorID {
+        if let Some(recycled_chunk_actor_id) = self.recycled_chunk_actor_ids.pop() {
+            recycled_chunk_actor_id
         } else {
-            let new_entity_id = self.current_entity_id;
-            self.current_entity_id = EntityID(new_entity_id.0 + 1);
+            let new_chunk_actor_id = self.current_chunk_actor_id;
+            self.current_chunk_actor_id = ChunkActorID(new_chunk_actor_id.0 + 1);
 
-            new_entity_id
+            new_chunk_actor_id
         }
     }
 
-    fn recycle_entity_id(&mut self, entity_id: EntityID) {
-        self.recycled_entity_ids.push(entity_id);
+    fn recycle_chunk_actor_id(&mut self, chunk_actor_id: ChunkActorID) {
+        self.recycled_chunk_actor_ids.push(chunk_actor_id);
     }
+}
+
+#[derive(Component)]
+struct Chunk {
+    id: ChunkID,
+    chunk_actors: Vec<ChunkActorID>,
 }
 
 #[derive(Component)]
@@ -184,38 +199,36 @@ struct ChunkLoader {
 }
 
 #[derive(Component)]
-struct Chunk {
-    id: ChunkID,
-    tracked_entities: Vec<EntityID>,
+struct ChunkActor {
+    id: ChunkActorID,
+    current_chunk: ChunkID,
+}
+
+#[derive(Component)]
+struct TranslationLerpFollower {
+    target: Entity,
+    smoothness: f32, // Higher values mean slower following (less smooth)
 }
 
 #[derive(Component)]
 struct Player;
-
-#[derive(Component)]
-struct TrackedEntity {
-    id: EntityID,
-    current_chunk: Option<ChunkID>,
-}
-
-#[derive(Component)]
-struct Follower {
-    target: Entity,
-    smoothness: f32, // Higher values mean slower following (less smooth)
-}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         .add_systems(Startup, main_setup_system)
-        .add_systems(Update, player_movement_system)
         .add_systems(Update, chunk_loader_system)
-        .add_systems(Update, follower_system)
+        .add_systems(Update, player_movement_system)
+        .add_systems(Update, player_creative_system)
+        .add_systems(Update, translation_lerp_follower_system)
         .run();
 }
 
-fn main_setup_system(mut commands: Commands) {
+fn main_setup_system(mut commands: Commands, mut rapier_configuration: ResMut<RapierConfiguration>) {
+    // Rapier Configuration
+    rapier_configuration.gravity = Vec2::new(0.0, 0.0);
+    
     // Player entity
     let player_entity = commands.spawn(Player)
     .insert(SpriteBundle {
@@ -234,52 +247,27 @@ fn main_setup_system(mut commands: Commands) {
     .id();
     
     // Universe manager
-    commands.insert_resource(UniverseManager { 
+    commands.insert_resource(ChunkManager { 
         registered_chunks: Vec::new(), 
         loaded_chunks: HashMap::new(), 
-        current_entity_id: EntityID(0), 
-        recycled_entity_ids: Vec::new()
+        current_chunk_actor_id: ChunkActorID(0), 
+        recycled_chunk_actor_ids: Vec::new()
     });
 
     // Camera that follows the player
     let _camera_entity = commands.spawn(Camera2dBundle::default())
-    .insert(Follower { target: player_entity, smoothness: 0.1 })
+    .insert(TranslationLerpFollower { target: player_entity, smoothness: 0.1 })
     .id();
-}
-
-fn player_movement_system(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<(&mut Velocity, &Player)>,
-) {
-    let mut player_velocity = Vec2::new(0.0, 0.0);
-    let speed = 1000.0;
-
-    if keyboard_input.pressed(KeyCode::KeyW) {
-        player_velocity.y += 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::KeyS) {
-        player_velocity.y -= 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::KeyA) {
-        player_velocity.x -= 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::KeyD) {
-        player_velocity.x += 1.0;
-    }
-
-    for (mut velocity, _) in query.iter_mut() {
-        velocity.linvel = player_velocity.normalize_or_zero() * speed;
-    }
 }
 
 fn chunk_loader_system(
     mut commands: Commands,
     mut chunk_loader_query: Query<(&Transform, &mut ChunkLoader)>,
-    mut universe_manager: ResMut<UniverseManager>,
+    mut universe_manager: ResMut<ChunkManager>,
 ) {
     let (chunk_loader_transform, mut chunk_loader) = chunk_loader_query.single_mut();
-    let chunk_loader_entity_coordinate: EntityCoordinate = chunk_loader_transform.translation.into();
-    let current_chunk_coordinate: ChunkCoordinate = chunk_loader_entity_coordinate.into();
+    let chunk_loader_object_coordinate: ObjectCoordinate = chunk_loader_transform.translation.into();
+    let current_chunk_coordinate: ChunkCoordinate = chunk_loader_object_coordinate.into();
     let load_radius = chunk_loader.load_radius as i16;
     
     // Detect chunks around the player
@@ -343,7 +331,7 @@ fn chunk_loader_system(
 
 fn new_chunk_entity(commands: &mut Commands, chunk_id: ChunkID) -> Entity {
     let chunk_coordinate: ChunkCoordinate = chunk_id.into();
-    let chunk_entity_coordinate: EntityCoordinate = chunk_coordinate.into();
+    let chunk_object_coordinate: ObjectCoordinate = chunk_coordinate.into();
 
     let chunk_color = if (chunk_coordinate.0.x + chunk_coordinate.0.y) % 2 == 0 {
         Color::rgb(0.25, 0.25, 0.25)
@@ -352,14 +340,14 @@ fn new_chunk_entity(commands: &mut Commands, chunk_id: ChunkID) -> Entity {
     };
 
     let chunk_entity = commands.spawn((
-        Chunk { id: chunk_id, tracked_entities: Vec::new()},
+        Chunk { id: chunk_id, chunk_actors: Vec::new()},
         SpriteBundle {
             sprite: Sprite {
                 color: chunk_color,
                 custom_size: Some(Vec2::new(CHUNK_SIZE as f32, CHUNK_SIZE as f32)),
                 ..default()
             },
-            transform: Transform::from_translation(chunk_entity_coordinate.0),
+            transform: Transform::from_translation(chunk_object_coordinate.0),
             ..default()
         },
     )).id();
@@ -367,27 +355,111 @@ fn new_chunk_entity(commands: &mut Commands, chunk_id: ChunkID) -> Entity {
     chunk_entity
 }
 
-fn tracked_entity_system(
+fn chunk_actor_system(
     mut commands: Commands,
-    mut tracked_entity_query: Query<(Entity, &mut TrackedEntity, &Transform)>,
+    mut chunk_actor_query: Query<(Entity, &Transform, &mut ChunkActor)>,
     mut chunk_query: Query<(&mut Chunk)>,
-    universe_manager: Res<UniverseManager>,
+    universe_manager: Res<ChunkManager>,
 ) {
     // If a tracked entity has no current chunk and no chunk is found, do nothing
     // If a tracked entity has no current chunk and a chunk is found, add the entity to the chunk, and set the current chunk
     // If a tracked entity has a current chunk and that same chunk is found, do nothing
     // If a tracked entity has a current chunk and a different chunk is found, remove the entity from the current chunk, add it to the new chunk, and update the current chunk
     // If a tracked entity has a current chunk and no chunk is found, remove the entity from the current chunk and destroy the entity recursively
+
+    for (chunk_actor_entity, chunk_actor_transform, mut chunk_actor) in chunk_actor_query.iter_mut() {
+    }
 }
 
-fn follower_system(
-    mut follower_query: Query<(&mut Transform, &Follower)>,
-    target_query: Query<&Transform, Without<Follower>>
+fn translation_lerp_follower_system(
+    mut translation_lerp_follower_query: Query<(&mut Transform, &TranslationLerpFollower)>,
+    target_query: Query<&Transform, Without<TranslationLerpFollower>>
 ) {
-    for (mut transform, follower) in follower_query.iter_mut() {
-        if let Ok(target_transform) = target_query.get(follower.target) {
+    for (mut transform, translation_lerp_follower) in translation_lerp_follower_query.iter_mut() {
+        if let Ok(target_transform) = target_query.get(translation_lerp_follower.target) {
             let target_position = target_transform.translation;
-            transform.translation = transform.translation.lerp(target_position, 1.0 - follower.smoothness);
+            transform.translation = transform.translation.lerp(target_position, 1.0 - translation_lerp_follower.smoothness);
         }
     }
 }
+
+fn player_movement_system(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut Velocity, &Player)>,
+) {
+    let mut player_velocity = Vec2::new(0.0, 0.0);
+
+    if keyboard_input.pressed(KeyCode::KeyW) {
+        player_velocity.y += 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        player_velocity.y -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyA) {
+        player_velocity.x -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyD) {
+        player_velocity.x += 1.0;
+    }
+
+    for (mut velocity, _) in query.iter_mut() {
+        velocity.linvel = player_velocity.normalize_or_zero() * PLAYER_MOVEMENT_SPEED;
+    }
+}
+
+fn player_creative_system(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+    collider_query: Query<(Entity, &Collider, &Transform)>,
+    mut chunk_manager: ResMut<ChunkManager>,
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+) {
+    if let Ok(window) = window_query.get_single() {
+        if let Some(cursor_pos) = window.cursor_position() {
+            // Adjust for the y-coordinate to correctly map from screen to NDC
+            let window_size = Vec2::new(window.width(), window.height());
+            let cursor_pos_ndc = Vec2::new(
+                (cursor_pos.x / window_size.x) * 2.0 - 1.0, 
+                1.0 - (cursor_pos.y / window_size.y) * 2.0
+            );
+
+            if let Ok((camera, camera_transform)) = camera_query.get_single() {
+                let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
+                let world_pos = ndc_to_world.project_point3(cursor_pos_ndc.extend(-1.0)).truncate();
+                let chunk_object_coordinate: ObjectCoordinate = world_pos.into();
+                let chunk_coordinate: ChunkCoordinate = chunk_object_coordinate.into();
+                let chunk_id: ChunkID = chunk_coordinate.into();
+                let half_prop_size = PLAYER_CREATIVE_SQUARE_PROP_SIZE / 2.0;
+
+                // Place a new prop on right click
+                if mouse_button_input.just_pressed(MouseButton::Right) {
+                    commands.spawn(SpriteBundle {
+                        sprite: Sprite {
+                            color: Color::rgb(0.5, 0.5, 1.0),
+                            custom_size: Some(Vec2::splat(PLAYER_CREATIVE_SQUARE_PROP_SIZE)),
+                            ..default()
+                        },
+                        transform: Transform::from_translation(world_pos.extend(0.0)),
+                        ..default()
+                    })
+                    .insert(RigidBody::Dynamic)
+                    .insert(Collider::cuboid(half_prop_size, half_prop_size))
+                    .insert(ChunkActor { id: chunk_manager.get_unused_chunk_actor_id(), current_chunk: chunk_id });
+                }
+
+                // Delete props under the cursor on left click
+                if mouse_button_input.just_pressed(MouseButton::Left) {
+                    for (entity, _, transform) in collider_query.iter() {
+                        let collider_position = transform.translation.truncate();
+
+                        if (collider_position - world_pos).abs().max_element() < PLAYER_CREATIVE_SQUARE_PROP_SIZE / 2.0 {
+                            commands.entity(entity).despawn_recursive();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
