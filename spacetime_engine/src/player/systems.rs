cf257b3819674169e9b1a103e015cbf547317909
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2024-05-15 19:56:59 +0200

    WIP Prototype Mk5

--- spacetime_engine/src/player/systems.rs
+++ spacetime_engine/src/player/systems.rs
@@ -21,6 +21,11 @@ pub(in crate) fn startup(
 
     let spawn_chunk_entity = crate::chunk::functions::new_chunk_entity(&mut commands, spawn_chunk_id);
 
+    // TODO: This do make big error because not direct ecs world access used
+    // TODO: Use direct ECS world access instead of commands everywhere 
+    // TODO: Also reduce the nesting in the player creative systems
+    // TODO: Also implement/refactor & integrate custom position types for flat world position, deep world position(flat world position, but including the z-axis as a depth index) and chunk position, essentially generalizing chunk coordionate and chunk actor coordinate and other current coordinate/position types
+    // TODO: Refer back to the main.rs file for further, more high-level TODOs
     let mut spawn_chunk = chunk_query.get_mut(spawn_chunk_entity).unwrap();
     
     entity_registry.load_entity(spawn_chunk_entity_id, spawn_chunk_entity);
