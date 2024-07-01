d10cb753dd8d361e03b109385fd491e5dc2e7871
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2024-07-01 03:53:29 +0200

    WIP Prototype Mk5

--- spacetime_engine/src/chunk/loader/functions.rs
+++ spacetime_engine/src/chunk/loader/functions.rs
@@ -18,9 +18,9 @@ pub(super) fn upgrade_to_chunk_loader_entity(
     target_entity_reference: Entity,
 ) -> Result<Entity, Entity> {
     
-    let ineligible_entity_query_0 = world.query::<Entity, Without<Transform>>();
-    let ineligible_entity_query_1 = world.query::<Entity, With<ChunkLoader>>();
-    let eligible_entity_query = world.query::<Entity, (With<Transform>, Without<ChunkLoader>)>();
+    let mut ineligible_entity_query_0: QueryState<Entity, Without<Transform>> = world.query_filtered::<Entity, Without<Transform>>();
+    let mut ineligible_entity_query_1 = world.query_filtered::<Entity, With<ChunkLoader>>();
+    let mut eligible_entity_query = world.query_filtered::<Entity, (With<Transform>, Without<ChunkLoader>)>();
 
     if ineligible_entity_query_0.get(world, target_entity_reference).is_ok() {
         error!("Entity '{:?}' does not have a Transform component!", target_entity_reference);
@@ -35,7 +35,7 @@ pub(super) fn upgrade_to_chunk_loader_entity(
     };
 
     if let Ok(eligible_entity) = eligible_entity_query.get_mut(world, target_entity_reference) {
-        Ok(world.entity(eligible_entity).insert(ChunkLoader::new(chunk_loader_id, CHUNK_LOADER_LOAD_RADIUS)).id())
+        Ok(world.entity_mut(eligible_entity.clone()).insert(ChunkLoader::new(chunk_loader_id, CHUNK_LOADER_LOAD_RADIUS)).id())
     } else {
         error!("Entity does not exist or does not have a Transform component.");
 
