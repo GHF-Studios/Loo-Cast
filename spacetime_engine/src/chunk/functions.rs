418ca2b70a02669b51383505fed55ed408984089
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2024-07-09 00:07:56 +0200

    WIP Prototype Mk5

--- spacetime_engine/src/chunk/functions.rs
+++ spacetime_engine/src/chunk/functions.rs
@@ -197,32 +197,25 @@ pub(in crate) fn categorize_chunks(
         }
     }
 
-    // TODO: Fix bug and remove debug logging
-    // As long as chunks are logged as categorized AND filtered, the filtering is not working as intended
-
-    if !old_chunks.is_empty() || !new_chunks.is_empty() {
-        warn!("Categorized chunks");
-    }
-
     let old_chunks = old_chunks.into_iter().filter(|chunk_id| {
-        !chunk_registry.is_chunk_allocated(*chunk_id) ||
-        !chunk_registry.is_destroying_chunk(*chunk_id) || 
-        !chunk_registry.is_unloading_chunk(*chunk_id) || 
-        !chunk_loader.currently_destroying_chunks().contains(chunk_id) || 
-        !chunk_loader.currently_unloading_chunks().contains(chunk_id)
+        !(
+            chunk_registry.is_chunk_allocated(*chunk_id) ||
+            chunk_registry.is_destroying_chunk(*chunk_id) || 
+            chunk_registry.is_unloading_chunk(*chunk_id) || 
+            chunk_loader.currently_destroying_chunks().contains(chunk_id) || 
+            chunk_loader.currently_unloading_chunks().contains(chunk_id)
+        )
     }).collect::<Vec<_>>();
 
     let new_chunks = new_chunks.into_iter().filter(|chunk_id| {
-        !chunk_registry.is_chunk_allocated(*chunk_id) ||
-        !chunk_registry.is_creating_chunk(*chunk_id) || 
-        !chunk_registry.is_loading_chunk(*chunk_id) ||
-        !chunk_loader.currently_creating_chunks().contains(chunk_id) ||
-        !chunk_loader.currently_loading_chunks().contains(chunk_id)
+        !(
+            chunk_registry.is_chunk_allocated(*chunk_id) ||
+            chunk_registry.is_creating_chunk(*chunk_id) || 
+            chunk_registry.is_loading_chunk(*chunk_id) ||
+            chunk_loader.currently_creating_chunks().contains(chunk_id) ||
+            chunk_loader.currently_loading_chunks().contains(chunk_id)
+        )
     }).collect::<Vec<_>>();
-
-    if !old_chunks.is_empty() || !new_chunks.is_empty() {
-        warn!("Filtered chunks");
-    }
 
     (old_chunks, unchanged_chunks, new_chunks)
 }
