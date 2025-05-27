9236bdcae11b674a60837b315245ac86fbfeeede
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2025-05-28 00:11:00 +0200

    WIP Prototype Mk9

--- core_engine/src/lib.rs
+++ core_engine/src/lib.rs
@@ -151,7 +151,7 @@ fn startup_system() {
             shader_name: chunk_shader_name,
             shader_path: chunk_shader_path,
         });
-        //workflow!(Debug::SpawnDebugObjects);
+        workflow!(Debug::SpawnDebugObjects);
 
         //let chunk_coords: Vec<(i32, i32)> = (-8..=8)
         //    .flat_map(|x| (-8..=8).map(move |y| (x, y)))
