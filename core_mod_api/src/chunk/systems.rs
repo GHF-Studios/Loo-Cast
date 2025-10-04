a1cd122d728f7d770e853d7b301b5be003763257
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2025-10-04 20:16:40 +0200

    Revert "WIP Prototype Mk10"
    This reverts commit 9000f45fe4ab26c3dced7d55949278fc62b521c6.

--- core_mod_api/src/chunk/systems.rs
+++ core_mod_api/src/chunk/systems.rs
@@ -59,22 +59,6 @@ pub(crate) fn process_chunk_actions_system(
     mut workflow_handles: Local<Option<ChunkActionWorkflowHandles>>,
 ) {
     // Step 1: If workflows are running, wait for all to complete
-    if workflow_handles.is_none() && chunk_loader.zoom_state != ZoomState::None {
-        // No workflows to wait on, but zoom is pending
-        match chunk_loader.zoom_state {
-            ZoomState::ZoomIn => {
-                chunk_loader.id_mut().scale_mut().zoom_in();
-                println!("[ZOOM APPLY] No workflows needed. Applied Zoom In");
-            }
-            ZoomState::ZoomOut => {
-                chunk_loader.id_mut().scale_mut().zoom_out();
-                println!("[ZOOM APPLY] No workflows needed. Applied Zoom Out");
-            }
-            _ => {}
-        }
-        chunk_loader.zoom_state = ZoomState::None;
-    }
-
     if let Some(handles) = &mut *workflow_handles {
         let spawn_done = handles.spawn.as_ref().is_none_or(|h| h.is_finished());
         let despawn_done = handles.despawn.as_ref().is_none_or(|h| h.is_finished());
