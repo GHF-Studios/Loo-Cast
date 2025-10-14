b4d9bb80a027f52982c12ef463d86b9717f9a48d
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2025-10-14 12:17:21 +0200

    WIP Prototype Mk10

--- core_mod_api/src/usf/pos.rs
+++ core_mod_api/src/usf/pos.rs
@@ -336,6 +336,10 @@ impl SubgridPos {
 
     // TODO: Fix this to handle subgrid_offset properly
     pub fn zoom_out(&mut self) {
+        if self.grid_offset.parent.as_ref().unwrap().parent.is_none() {
+            panic!("Cannot zoom out SubgridPos beyond the root GridPos");
+        }
+
         let grid_pos = GridPos {
             parent: Some(Arc::new(self.grid_offset.clone())),
             scale: self.grid_offset.scale.zoomed_in(),
@@ -809,31 +813,51 @@ fn subgrid_pos_add_test_3() {
 }
 
 #[test]
-fn subgrid_pos_zoom_out_test_1() {
+fn subgrid_pos_sub_test_1() {
     let a = GridPos::new_root(IVec2::new(0, 0));
-    let a = SubgridPos::new(a, IVec2::new(4, 4));
-    let mut a = a;
-    a.zoom_out();
-    let expected = GridPos::new_root(IVec2::new(0, 0));
-    assert_eq!(a.grid_offset, expected);
-    assert_eq!(a.subgrid_offset, IVec2::new(0, 0));
+    let a = SubgridPos::new(a, IVec2::new(-3, -3));
+    let b = GridPos::new_root(IVec2::new(0, 0));
+    let b = SubgridPos::new(b, IVec2::new(4, 4));
+    let c = a - b;
+    let expected_grid = GridPos::new_root(IVec2::new(-1, -1));
+    let expected = SubgridPos::new(expected_grid, IVec2::new(3, 3));
+    assert_eq!(c, expected);
 }
 
 #[test]
-fn subgrid_pos_zoom_out_test_2() {
+fn subgrid_pos_sub_test_2() {
     let a = GridPos::new_root(IVec2::new(0, 0));
     let a = GridPos::new(a, IVec2::new(4, 4));
-    let a = SubgridPos::new(a, IVec2::new(3, 3));
-    let mut a = a;
-    a.zoom_out();
+    let a = GridPos::new(a, IVec2::new(3, 3));
+    let a = SubgridPos::new(a, IVec2::new(2, 2));
+    let b = GridPos::new_root(IVec2::new(0, 0));
+    let b = SubgridPos::new(b, IVec2::new(1, 1));
+    let c = a - b;
     let expected_grid = GridPos::new_root(IVec2::new(0, 0));
-    let expected_grid = GridPos::new(expected_grid, IVec2::new(4, 4));
-    assert_eq!(a.grid_offset, expected_grid);
-    assert_eq!(a.subgrid_offset, IVec2::new(0, 0));
+    let expected_grid = GridPos::new(expected_grid, IVec2::new(3, 3));
+    let expected_grid = GridPos::new(expected_grid, IVec2::new(3, 3));
+    let expected = SubgridPos::new(expected_grid, IVec2::new(2, 2));
+    assert_eq!(c, expected);
 }
 
 #[test]
-fn subgrid_pos_zoom_out_test_3() {
+fn subgrid_pos_sub_test_3() {
+    let a = GridPos::new_root(IVec2::new(0, 0));
+    let a = SubgridPos::new(a, IVec2::new(1, 1));
+    let b = GridPos::new_root(IVec2::new(0, 0));
+    let b = GridPos::new(b, IVec2::new(4, 4));
+    let b = GridPos::new(b, IVec2::new(3, 3));
+    let b = SubgridPos::new(b, IVec2::new(2, 2));
+    let c = a - b;
+    let expected_grid = GridPos::new_root(IVec2::new(0, 0));
+    let expected_grid = GridPos::new(expected_grid, IVec2::new(-3, -3));
+    let expected_grid = GridPos::new(expected_grid, IVec2::new(-3, -3));
+    let expected = SubgridPos::new(expected_grid, IVec2::new(-2, -2));
+    assert_eq!(c, expected);
+}
+
+#[test]
+fn subgrid_pos_zoom_out_test_1() {
     let a = GridPos::new_root(IVec2::new(0, 0));
     let a = GridPos::new(a, IVec2::new(4, 4));
     let a = GridPos::new(a, IVec2::new(3, 3));
@@ -842,7 +866,40 @@ fn subgrid_pos_zoom_out_test_3() {
     a.zoom_out();
     let expected_grid = GridPos::new_root(IVec2::new(0, 0));
     let expected_grid = GridPos::new(expected_grid, IVec2::new(4, 4));
+    let expected = SubgridPos::new(expected_grid, IVec2::new(3, 3));
+    assert_eq!(a, expected);
+}
+
+#[test]
+fn subgrid_pos_zoom_out_test_2() {
+    let a = GridPos::new_root(IVec2::new(0, 0));
+    let a = GridPos::new(a, IVec2::new(4, 4));
+    let a = GridPos::new(a, IVec2::new(3, 3));
+    let a = GridPos::new(a, IVec2::new(2, 2));
+    let a = SubgridPos::new(a, IVec2::new(1, 1));
+    let mut a = a;
+    a.zoom_out();
+    let expected_grid = GridPos::new_root(IVec2::new(0, 0));
+    let expected_grid = GridPos::new(expected_grid, IVec2::new(4, 4));
     let expected_grid = GridPos::new(expected_grid, IVec2::new(3, 3));
-    assert_eq!(a.grid_offset, expected_grid);
-    assert_eq!(a.subgrid_offset, IVec2::new(0, 0));
+    let expected = SubgridPos::new(expected_grid, IVec2::new(2, 2));
+    assert_eq!(a, expected);
+}
+
+#[test]
+fn subgrid_pos_zoom_out_test_3() {
+    let a = GridPos::new_root(IVec2::new(0, 0));
+    let a = GridPos::new(a, IVec2::new(4, 4));
+    let a = GridPos::new(a, IVec2::new(3, 3));
+    let a = GridPos::new(a, IVec2::new(2, 2));
+    let a = GridPos::new(a, IVec2::new(1, 1));
+    let a = SubgridPos::new(a, IVec2::new(0, 0));
+    let mut a = a;
+    a.zoom_out();
+    let expected_grid = GridPos::new_root(IVec2::new(0, 0));
+    let expected_grid = GridPos::new(expected_grid, IVec2::new(4, 4));
+    let expected_grid = GridPos::new(expected_grid, IVec2::new(3, 3));
+    let expected_grid = GridPos::new(expected_grid, IVec2::new(2, 2));
+    let expected = SubgridPos::new(expected_grid, IVec2::new(1, 1));
+    assert_eq!(a, expected);
 }
