0871e7d3e9468c905324428fac8936a5e9437b26
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2025-11-12 14:59:38 +0100

    WIP Prototype Mk10

--- core_mod_api/src/ui/custom_perf_ui_entries/cursor_position.rs
+++ core_mod_api/src/ui/custom_perf_ui_entries/cursor_position.rs
@@ -1,14 +1,17 @@
 use bevy::prelude::*;
 use bevy::ecs::system::SystemParam;
+use bevy::picking::pointer::{Location, PointerAction, PointerButton, PointerId, PointerInput, PointerLocation, PointerPress};
 use bevy::window::PrimaryWindow;
 use iyes_perf_ui::{entry::PerfUiEntry, ui::root::PerfUiRoot, utils::next_sort_key};
 
-use crate::camera::components::MainCamera;
+use crate::{camera::components::MainCamera, picking::constants::MOUSE_POINTER_ID};
 
 #[derive(Bundle, Default, Reflect)]
 pub struct PerfUiCursorPosEntries {
     pub screen_pos: PerfUiEntryCursorScreenPos,
     pub unit_pos: PerfUiEntryCursorUnitPos,
+    pub ingame_picking_screen_pos: PerfUiEntryGamePickingCursorScreenPos,
+    pub indebug_picking_screen_pos: PerfUiEntryDebugPickingCursorScreenPos,
 }
 
 #[derive(Component, Debug, Clone, Reflect)]
@@ -105,6 +108,146 @@ impl PerfUiEntry for PerfUiEntryCursorUnitPos {
 
     fn value_color(&self, _value: &Self::Value) -> Option<Color> {
         Some(Color::linear_rgba(0.0, 0.0, 1.0, 1.0))
+    }
+
+    fn value_highlight(&self, _value: &Self::Value) -> bool {
+        false
+    }
+}
+
+#[derive(Component, Debug, Clone, Reflect)]
+#[require(PerfUiRoot)]
+pub struct PerfUiEntryGamePickingCursorScreenPos {
+    pub label: String,
+    pub sort_key: i32,
+}
+impl Default for PerfUiEntryGamePickingCursorScreenPos {
+    fn default() -> Self {
+        Self {
+            label: "(GPC)Pos".to_string(),
+            sort_key: next_sort_key(),
+        }
+    }
+}
+impl PerfUiEntry for PerfUiEntryGamePickingCursorScreenPos {
+    type SystemParam = EventReader<'static, 'static, Pointer<Move>>;
+    type Value = Vec2;
+
+    fn label(&self) -> &str {
+        &self.label
+    }
+
+    fn sort_key(&self) -> i32 {
+        self.sort_key
+    }
+
+    fn update_value(
+        &self,
+        query: &mut <Self::SystemParam as SystemParam>::Item<'_, '_>,
+    ) -> Option<Self::Value> {
+        static PREVIOUS_POS: std::sync::Mutex<Option<Vec2>> = std::sync::Mutex::new(None);
+        
+        let mut previous_position = PREVIOUS_POS.lock().unwrap();
+
+        let maybe_pos = query
+            .read()
+            .filter(|e| e.pointer_id == PointerId::Mouse)
+            .last()
+            .and_then(|event| event.hit.position.map(|p| p.truncate()));
+
+        match (maybe_pos, *previous_position) {
+            (None, None) => None,
+            (Some(pos), None) => {
+                *previous_position = Some(pos);
+                Some(pos)
+            },
+            (None, Some(prev_pos)) => {
+                *previous_position = None;
+                Some(prev_pos)
+            },
+            (Some(pos), Some(_)) => {
+                *previous_position = Some(pos);
+                Some(pos)
+            },
+        }
+    }
+
+    fn format_value(&self, value: &Self::Value) -> String {
+        format!("{:.1}", value)
+    }
+
+    fn value_color(&self, _value: &Self::Value) -> Option<Color> {
+        Some(Color::linear_rgba(1.0, 0.0, 0.0, 1.0))
+    }
+
+    fn value_highlight(&self, _value: &Self::Value) -> bool {
+        false
+    }
+}
+
+#[derive(Component, Debug, Clone, Reflect)]
+#[require(PerfUiRoot)]
+pub struct PerfUiEntryDebugPickingCursorScreenPos {
+    pub label: String,
+    pub sort_key: i32,
+}
+impl Default for PerfUiEntryDebugPickingCursorScreenPos {
+    fn default() -> Self {
+        Self {
+            label: "(DPC)Pos".to_string(),
+            sort_key: next_sort_key(),
+        }
+    }
+}
+impl PerfUiEntry for PerfUiEntryDebugPickingCursorScreenPos {
+    type SystemParam = EventReader<'static, 'static, Pointer<Move>>;
+    type Value = Vec2;
+
+    fn label(&self) -> &str {
+        &self.label
+    }
+
+    fn sort_key(&self) -> i32 {
+        self.sort_key
+    }
+
+    fn update_value(
+        &self,
+        query: &mut <Self::SystemParam as SystemParam>::Item<'_, '_>,
+    ) -> Option<Self::Value> {
+        static PREVIOUS_POS: std::sync::Mutex<Option<Vec2>> = std::sync::Mutex::new(None);
+
+        let mut previous_position = PREVIOUS_POS.lock().unwrap();
+
+        let maybe_pos = query
+            .read()
+            .filter(|e| e.pointer_id == MOUSE_POINTER_ID)
+            .last()
+            .and_then(|event| event.hit.position.map(|p| p.truncate()));
+
+        match (maybe_pos, *previous_position) {
+            (None, None) => None,
+            (Some(pos), None) => {
+                *previous_position = Some(pos);
+                Some(pos)
+            },
+            (None, Some(prev_pos)) => {
+                *previous_position = None;
+                Some(prev_pos)
+            },
+            (Some(pos), Some(_)) => {
+                *previous_position = Some(pos);
+                Some(pos)
+            },
+        }
+    }
+
+    fn format_value(&self, value: &Self::Value) -> String {
+        format!("{:.1}", value)
+    }
+
+    fn value_color(&self, _value: &Self::Value) -> Option<Color> {
+        Some(Color::linear_rgba(1.0, 0.0, 0.0, 1.0))
     }
 
     fn value_highlight(&self, _value: &Self::Value) -> bool {
--- core_mod_api/src/ui/custom_perf_ui_entries/mod.rs
+++ core_mod_api/src/ui/custom_perf_ui_entries/mod.rs
@@ -3,7 +3,7 @@ pub mod player_position;
 
 use bevy::prelude::*;
 use iyes_perf_ui::PerfUiAppExt;
-use cursor_position::{PerfUiEntryCursorScreenPos, PerfUiEntryCursorUnitPos};
+use cursor_position::{PerfUiEntryCursorScreenPos, PerfUiEntryCursorUnitPos, PerfUiEntryGamePickingCursorScreenPos, PerfUiEntryDebugPickingCursorScreenPos};
 use player_position::{PerfUiEntryPlayerGridPos, PerfUiEntryPlayerUnitPos};
 
 pub(crate) struct CustomPerfUiEntriesPlugin;
@@ -14,9 +14,13 @@ impl Plugin for CustomPerfUiEntriesPlugin {
             .add_perf_ui_simple_entry::<PerfUiEntryCursorUnitPos>()
             .add_perf_ui_simple_entry::<PerfUiEntryPlayerGridPos>()
             .add_perf_ui_simple_entry::<PerfUiEntryPlayerUnitPos>()
+            .add_perf_ui_simple_entry::<PerfUiEntryGamePickingCursorScreenPos>()
+            .add_perf_ui_simple_entry::<PerfUiEntryDebugPickingCursorScreenPos>()
             .register_type::<PerfUiEntryCursorScreenPos>()
             .register_type::<PerfUiEntryCursorUnitPos>()
             .register_type::<PerfUiEntryPlayerGridPos>()
-            .register_type::<PerfUiEntryPlayerUnitPos>();
+            .register_type::<PerfUiEntryPlayerUnitPos>()
+            .register_type::<PerfUiEntryGamePickingCursorScreenPos>()
+            .register_type::<PerfUiEntryDebugPickingCursorScreenPos>();
     }
 }
