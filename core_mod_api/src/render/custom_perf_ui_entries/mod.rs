0e90f5d118ecc1e84227c7c8f404e5a47ba8a4b3
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2025-11-18 17:15:43 +0100

    WIP Prototype Mk10

--- core_mod_api/src/render/custom_perf_ui_entries/mod.rs
+++ core_mod_api/src/render/custom_perf_ui_entries/mod.rs
@@ -3,23 +3,25 @@ pub mod player_position;
 
 use bevy::prelude::*;
 use iyes_perf_ui::PerfUiAppExt;
-use cursor_position::{PerfUiEntryCursorWindowPos, PerfUiEntryCursorUnitPos, PerfUiEntryCursorPointerPos, PerfUiEntryViewportRect};
+use cursor_position::{PerfUiEntryViewportRect, PerfUiEntryCursorWindowPos, PerfUiEntryCursorPointerPos, PerfUiEntryCursorUnitPos, PerfUiEntryCursorUnitPosUnprojected};
 use player_position::{PerfUiEntryPlayerGridPos, PerfUiEntryPlayerUnitPos};
 
 pub(crate) struct CustomPerfUiEntriesPlugin;
 impl Plugin for CustomPerfUiEntriesPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_perf_ui_simple_entry::<PerfUiEntryCursorWindowPos>()
-            .add_perf_ui_simple_entry::<PerfUiEntryCursorUnitPos>()
-            .add_perf_ui_simple_entry::<PerfUiEntryCursorPointerPos>()
             .add_perf_ui_simple_entry::<PerfUiEntryViewportRect>()
+            .add_perf_ui_simple_entry::<PerfUiEntryCursorWindowPos>()
+            .add_perf_ui_simple_entry::<PerfUiEntryCursorPointerPos>()
+            .add_perf_ui_simple_entry::<PerfUiEntryCursorUnitPos>()
+            .add_perf_ui_simple_entry::<PerfUiEntryCursorUnitPosUnprojected>()
             .add_perf_ui_simple_entry::<PerfUiEntryPlayerGridPos>()
             .add_perf_ui_simple_entry::<PerfUiEntryPlayerUnitPos>()
-            .register_type::<PerfUiEntryCursorWindowPos>()
-            .register_type::<PerfUiEntryCursorUnitPos>()
-            .register_type::<PerfUiEntryCursorPointerPos>()
             .register_type::<PerfUiEntryViewportRect>()
+            .register_type::<PerfUiEntryCursorWindowPos>()
+            .register_type::<PerfUiEntryCursorPointerPos>()
+            .register_type::<PerfUiEntryCursorUnitPos>()
+            .register_type::<PerfUiEntryCursorUnitPosUnprojected>()
             .register_type::<PerfUiEntryPlayerGridPos>()
             .register_type::<PerfUiEntryPlayerUnitPos>();
     }
