43e044ac71d6dd04ee4968acaf4f8b9fd75d6338
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2025-12-22 20:57:51 +0100

    WIP Prototype Mk10
    Working on this weird bug: When you spawn in, and zoom in once, we seem to oscillate? Very weird

--- core_mod_api/src/chunk/systems.rs
+++ core_mod_api/src/chunk/systems.rs
@@ -64,25 +64,31 @@ pub(crate) fn chunk_detection_system(
     let mut target_chunk_cone = Vec::new();
 
     while chunk_loader_scale_cursor < Scale::MAX {
-        warn!("Chunk Detection at scale: {:?}", chunk_loader_scale_cursor);
+        // warn!("Chunk Detection at scale: {:?}", chunk_loader_scale_cursor);
     
         let coords_in_radius = chunk_loader_grid_coord_cursor
             .query_grid_radius(radius)
             .into_iter()
             .collect::<HashSet<GridVec>>();
+        // warn!("Detected Chunks: {:?}", coords_in_radius);
         target_chunk_cone.push((chunk_loader_grid_coord_cursor.clone(), coords_in_radius));
         chunk_loader_scale_cursor.zoom_out();
         chunk_loader_grid_coord_cursor = &**chunk_loader_grid_coord_cursor.parent.as_ref().unwrap();
     }
     
-    warn!("Final Chunk Detection at scale: {:?}", chunk_loader_scale_cursor);
+    // warn!("Final Chunk Detection at scale: {:?}", chunk_loader_scale_cursor);
     let coords_in_radius = chunk_loader_grid_coord_cursor
         .query_grid_radius(radius)
         .into_iter()
         .collect::<HashSet<GridVec>>();
+    // warn!("Detected Chunks: {:?}", coords_in_radius);
     target_chunk_cone.push((chunk_loader_grid_coord_cursor.clone(), coords_in_radius));
 
     target_chunk_cone.reverse();
+
+    for (chunk_loader_grid_coord, target_chunks) in &target_chunk_cone {
+        warn!("Target Chunks at scale: {:?} => {{{}}} {:?}", chunk_loader_grid_coord.scale, target_chunks.len(), target_chunks);
+    }
 
     for (_chunk_loader_grid_coord, target_chunks) in target_chunk_cone {
         let chunks_to_load: Vec<_> = target_chunks.difference(&current_chunks).cloned().collect();
