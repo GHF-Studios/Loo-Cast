074d6347e0696e8be89fe1f9dc50c10c4b473fd3
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2026-01-18 14:13:23 +0100

    WIP Prototype Mk10

--- core_mod_api/src/script/core/internals/types.rs
+++ core_mod_api/src/script/core/internals/types.rs
@@ -1,5 +1,42 @@
 use rhai::Shared;
 use std::sync::{Arc, Mutex, MutexGuard};
+use std::sync::atomic::{AtomicBool, Ordering};
+
+pub struct Handle<T> {
+    value: Mutex<Option<T>>,
+    is_valid: AtomicBool,
+}
+
+impl<T> Handle<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            value: Mutex::new(Some(value)),
+            is_valid: AtomicBool::new(true),
+        }
+    }
+
+    pub fn invalidate(&self) {
+        self.is_valid.store(false, Ordering::SeqCst);
+    }
+
+    pub fn take(&self) -> Option<T> {
+        self.value.lock().ok()?.take()
+    }
+
+    pub fn try_access(&self) -> Option<MutexGuard<'_, Option<T>>> {
+        if !self.is_valid.load(Ordering::SeqCst) {
+            return None;
+        }
+        self.value.try_lock().ok()
+    }
+}
+
+pub trait SelfAccess<T> {
+    fn raw_access<F, R>(&self, f: F) -> Result<R, &'static str>
+    where
+        F: FnOnce(&mut T) -> R;
+}
+
 
 pub type ScopedAccessHandle<T> = Shared<Mutex<ScopedAccess<T>>>;
 
